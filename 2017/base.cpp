cb217b5e3c59365653cce63e4dba0b6b1e405dd1
Augusto Cesar <augusto.acfs@gmail.com>
2020-12-17 21:15:03 +0100

    2017: Day 2: Added
    Plus added a split function to the base.cpp

--- 2017/base.cpp
+++ 2017/base.cpp
@@ -2,6 +2,8 @@
 #include <sstream>
 #include <fstream>
 #include <vector>
+#include <string>
+#include <algorithm>
 
 #define c_to_digit(c) (c - '0')
 
@@ -14,4 +16,16 @@ std::string read_input(std::string path)
         ;
 
     return sstr.str();
+}
+
+std::vector<std::string> split(std::string str, char delimiter)
+{
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(str);
+
+    while (std::getline(tokenStream, token, delimiter))
+        tokens.push_back(token);
+
+    return tokens;
 }
