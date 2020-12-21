8c454b12db9bab0d69755079d6e9a10728bbdb4a
Augusto Cesar <augusto.acfs@gmail.com>
2020-12-21 23:29:59 +0100

    2020: Day 21: Added

---
+++ 2020/day21/day21.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type recipe struct {
+	ingredients []string
+	allergens   []string
+}
+
+var lineRegex = regexp.MustCompile(`(.*)\s\(contains\s(.*)\)`)
+
+func partOne() {
+	recipes := parseInput()
+	ingredients, _, ingredientAllergen := processRecipes(recipes)
+
+	sum := 0
+	for _, ingredient := range ingredients {
+		if _, ok := ingredientAllergen[ingredient]; ok {
+			continue
+		}
+
+		sum++
+	}
+
+	fmt.Printf("Part One: %d\n", sum)
+}
+
+func partTwo() {
+	recipes := parseInput()
+	_, _, ingredientAllergen := processRecipes(recipes)
+
+	allergenIngredient := map[string]string{}
+	for k, v := range ingredientAllergen {
+		allergenIngredient[v] = k
+	}
+
+	sortedAllergens := mapKeys(allergenIngredient)
+	sort.Strings(sortedAllergens)
+
+	result := []string{}
+	for _, key := range sortedAllergens {
+		result = append(result, allergenIngredient[key])
+	}
+
+	fmt.Printf("Part Two: %s\n", strings.Join(result, ","))
+}
+
+func processRecipes(recipes []recipe) (ingredients []string, allergens []string, ingredientAllergen map[string]string) {
+	// Allergen -> []Ingredient
+	allergenPossibleIngredients := map[string][]string{}
+	ingredientAllergen = map[string]string{}
+
+	allergens = []string{}
+	ingredients = []string{}
+
+	for _, recipe := range recipes {
+		for _, allergen := range recipe.allergens {
+			if _, ok := allergenPossibleIngredients[allergen]; !ok {
+				allergens = append(allergens, allergen)
+				allergenPossibleIngredients[allergen] = recipe.ingredients
+				continue
+			}
+
+			allergenPossibleIngredients[allergen] = matches(allergenPossibleIngredients[allergen], recipe.ingredients)
+		}
+
+		ingredients = append(ingredients, recipe.ingredients...)
+	}
+
+	for len(allergenPossibleIngredients) > 0 {
+		for allergen, aIngredients := range allergenPossibleIngredients {
+			if len(aIngredients) == 1 {
+				ingredientAllergen[aIngredients[0]] = allergen
+
+				removeFromAll(&allergenPossibleIngredients, aIngredients[0])
+				delete(allergenPossibleIngredients, allergen)
+			}
+		}
+	}
+
+	return
+}
+
+func parseInput() []recipe {
+	lines := strings.Split(readInput(), "\n")
+	recipes := make([]recipe, len(lines))
+
+	for i, line := range lines {
+		match := lineRegex.FindStringSubmatch(line)
+		ingredients := strings.Split(match[1], " ")
+		allergens := strings.Split(match[2], ", ")
+		recipes[i] = recipe{ingredients: ingredients, allergens: allergens}
+	}
+
+	return recipes
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// TODO: Create a shared file for these functions. Pretty sure I'm rewriting a bunch of them
+
+func matches(sliceA []string, sliceB []string) []string {
+	res := []string{}
+	for _, item := range sliceA {
+		if contains(sliceB, item) {
+			res = append(res, item)
+		}
+	}
+	return res
+}
+
+func contains(slice []string, value string) bool {
+	for _, item := range slice {
+		if item == value {
+			return true
+		}
+	}
+
+	return false
+}
+
+func removeFromAll(m *map[string][]string, value string) {
+	nm := *m
+
+	for k, v := range nm {
+		newList := []string{}
+		for _, item := range v {
+			if item != value {
+				newList = append(newList, item)
+			}
+		}
+		nm[k] = newList
+	}
+
+	*m = nm
+}
+
+func mapKeys(m map[string]string) []string {
+	res := make([]string, len(m))
+	i := 0
+	for k := range m {
+		res[i] = k
+		i++
+	}
+
+	return res
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+func main() {
+	partOne()
+	partTwo()
+}
+
+func readInput() string {
+	input, err := ioutil.ReadFile("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	return string(input)
+}
+
+func atoi(str string) int {
+	val, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+	}
+
+	return val
+}
