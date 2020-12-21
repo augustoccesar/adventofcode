2c048216cf629681ba247adb2735f53a429402d6
Augusto Cesar <augusto.acfs@gmail.com>
2020-12-22 00:31:56 +0100

    2020: Day 18: Use utils

--- 2020/day18/day18.go
+++ 2020/day18/day18.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"regexp"
 	"strconv"
 	"strings"
+
+	"github.com/augustoccesar/adventofcode/utils"
 )
 
 var deepestOperationPattern = regexp.MustCompile(`\([^\(\)]+\)`) // Parenthesis that don't contain other parenthesis inside
@@ -21,7 +22,7 @@ func calculate(str string, inverse bool) int {
 		expression := str[start+1 : end-1]
 		result := calculate(expression, inverse)
 
-		str = replace(str, strconv.Itoa(result), start, end-1)
+		str = utils.StringReplace(str, strconv.Itoa(result), start, end-1)
 	}
 
 	// If is inverse, calculate the sums first
@@ -34,16 +35,16 @@ func calculate(str string, inverse bool) int {
 			expression := str[start:end]
 			result := calculate(expression, false)
 
-			str = replace(str, strconv.Itoa(result), start, end-1)
+			str = utils.StringReplace(str, strconv.Itoa(result), start, end-1)
 		}
 	}
 
 	items := strings.Fields(str)
-	calcResult := atoi(items[0])
+	calcResult := utils.Atoi(items[0])
 
 	for i := 1; i < len(items); i += 2 {
 		op := items[i]
-		nextItem := atoi(items[i+1])
+		nextItem := utils.Atoi(items[i+1])
 
 		if op == "+" {
 			calcResult += nextItem
@@ -58,7 +59,7 @@ func calculate(str string, inverse bool) int {
 }
 
 func partOne() {
-	expressions := strings.Split(readInput(), "\n")
+	expressions := strings.Split(utils.ReadFile("./input.txt"), "\n")
 
 	sum := 0
 	for _, exp := range expressions {
@@ -69,7 +70,7 @@ func partOne() {
 }
 
 func partTwo() {
-	expressions := strings.Split(readInput(), "\n")
+	expressions := strings.Split(utils.ReadFile("./input.txt"), "\n")
 
 	sum := 0
 	for _, exp := range expressions {
@@ -79,64 +80,9 @@ func partTwo() {
 	fmt.Printf("Part Two: %d\n", sum)
 }
 
-// There must be a better way of doing this xD
-// replace replaces a string with a substring from defined points (both inclusive) on the input string
-// Example:
-//   input = "a b c d e f"
-//   replace = "x"
-//   start = 4
-//   end = 6
-//
-//   result = "a b x e f"
-func replace(input string, replace string, start, end int) string {
-	inputRunes := []rune(input)
-	replaceRunes := []rune(replace)
-
-	newSize := len(inputRunes) + len(replace) - ((end - start) + 1)
-
-	result := make([]rune, newSize)
-
-	for i, j := 0, 0; i < len(inputRunes); i, j = i+1, j+1 {
-		if i != start {
-			result[j] = inputRunes[i]
-			continue
-		}
-
-		if i == start {
-			for l := 0; l < len(replaceRunes); l++ {
-				result[i+l] = replaceRunes[l]
-			}
-
-			j += len(replaceRunes) - 1
-			i = end
-		}
-	}
-
-	res := string(result)
-	return res
-}
-
 // --------------------------------------------------------------------------------------------------------------------
 
 func main() {
 	partOne()
 	partTwo()
-}
-
-func readInput() string {
-	input, err := ioutil.ReadFile("./input.txt")
-	if err != nil {
-		panic(err)
-	}
-
-	return string(input)
-}
-
-func atoi(str string) int {
-	val, err := strconv.Atoi(str)
-	if err != nil {
-		panic(err)
-	}
-
-	return val
 }
