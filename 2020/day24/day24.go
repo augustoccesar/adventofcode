23df0af6882c1dace0ae361bc2842c1010e7015d
Augusto Cesar <augusto.acfs@gmail.com>
2020-12-24 17:55:12 +0100

    2020: Day 24: Added part two

--- 2020/day24/day24.go
+++ 2020/day24/day24.go
@@ -10,9 +10,6 @@ import (
 
 func partOne() {
 	inputLines := strings.Split(utils.ReadFile("./input.txt"), "\n")
-	// NW  _  NE
-	//  W  C  E
-	// SW  _  SE
 
 	tilesMap := map[string]*Tile{}
 	for _, line := range inputLines {
@@ -38,9 +35,54 @@ func partOne() {
 }
 
 func partTwo() {
+	inputLines := strings.Split(utils.ReadFile("./input.txt"), "\n")
+
+	blackTiles := map[string]*Tile{}
+	for _, line := range inputLines {
+		tile := NewTile()
+
+		for _, step := range linePattern.FindAllString(line, -1) {
+			tile.Move(step)
+		}
+
+		if _, ok := blackTiles[tile.ID()]; !ok {
+			blackTiles[tile.ID()] = tile
+		} else {
+			delete(blackTiles, tile.ID())
+		}
+	}
+
+	for i := 0; i < 100; i++ {
+		blackNeighborsCount := make(map[string]int)
+
+		for id := range blackTiles {
+			for _, nID := range blackTiles[id].NeighborsIDs() {
+				blackNeighborsCount[nID]++
+			}
+		}
+
+		newBlacks := map[string]*Tile{}
+
+		for id, blacks := range blackNeighborsCount {
+			_, isBlack := blackTiles[id]
+			if isBlack && blacks != 0 && blacks <= 2 { // Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white. (AKA not black anymore)
+				newBlacks[id] = blackTiles[id]
+			} else if !isBlack && blacks == 2 { // Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
+				newBlacks[id] = NewTileWithCoords(tileIDToCoords(id))
+			}
+		}
+
+		blackTiles = newBlacks
+	}
+
+	fmt.Printf("Day Two: %d\n", len(blackTiles))
 }
 
 // --------------------------------------------------------------------------------------------------------------------
+
+// NW  _  NE
+//  W  C  E
+// SW  _  SE
 
 var linePattern = regexp.MustCompile(`w|e|se|ne|sw|nw`)
 
@@ -59,6 +101,8 @@ const (
 	ColorWhite = "w"
 )
 
+// https://en.wikipedia.org/wiki/Hexagonal_tiling
+// https://stackoverflow.com/questions/2459402/hexagonal-grid-coordinates-to-pixel-coordinates
 type Tile struct {
 	X     int
 	Y     int
@@ -70,8 +114,12 @@ func NewTile() *Tile {
 	return &Tile{X: 0, Y: 0, Z: 0, Color: ColorWhite}
 }
 
+func NewTileWithCoords(x, y, z int) *Tile {
+	return &Tile{X: x, Y: y, Z: z, Color: ColorWhite}
+}
+
 func (t *Tile) ID() string {
-	return fmt.Sprintf("%d,%d,%d", t.X, t.Y, t.Z)
+	return coordsToID(t.X, t.Y, t.Z)
 }
 
 func (t *Tile) Flip() {
@@ -116,6 +164,34 @@ func (t *Tile) Move(dir Direction) {
 		t.Z = t.Z - 1
 		break
 	}
+}
+
+func (t *Tile) NeighborsIDs() []string {
+	ids := make([]string, 6)
+	coordsFromPoint := [][]int{
+		{-1, 0, 1}, // NW
+		{-1, 1, 0}, // W
+		{0, 1, -1}, // SW
+		{0, -1, 1}, // NE
+		{1, -1, 0}, // E
+		{1, 0, -1}, // SE
+	}
+
+	for i, coord := range coordsFromPoint {
+		ids[i] = coordsToID(t.X+coord[0], t.Y+coord[1], t.Z+coord[2])
+	}
+
+	return ids
+}
+
+func coordsToID(x, y, z int) string {
+	return fmt.Sprintf("%d,%d,%d", x, y, z)
+}
+
+func tileIDToCoords(id string) (x, y, z int) {
+	coord := strings.Split(id, ",")
+	x, y, z = utils.Atoi(coord[0]), utils.Atoi(coord[1]), utils.Atoi(coord[2])
+	return
 }
 
 // --------------------------------------------------------------------------------------------------------------------
