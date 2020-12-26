52aa3a6cccee6b52a07c92792e64a67ba07de830
Augusto Cesar <augusto.acfs@gmail.com>
2020-12-26 19:01:02 +0100

    2016: Day 01: Change approach + add part two
    - Add ds_store to gitignore

--- 2016/src/day01/mod.rs
+++ 2016/src/day01/mod.rs
@@ -1,9 +1,68 @@
+use std::collections::HashMap;
+use std::convert::TryFrom;
 use std::fs;
 
 use crate::task::Task;
 
-// const directions: [&str; 4] = ["n", "e", "s", "w"];
-const DIRECTIONS: [(i8, i8); 4] = [
+pub struct Day01 {}
+
+impl Task for Day01 {
+    fn part_one(&self) {
+        let input = fs::read_to_string("inputs/day01_input.txt").unwrap();
+        let commands = input
+            .split(", ")
+            .map(|cmd| Command::parse(cmd))
+            .collect::<Vec<Command>>();
+        let mut facing: usize = 0;
+        let mut point: Point2D = Point2D::new(0, 0);
+
+        for command in commands {
+            update_facing(&mut facing, command.direction);
+            let direction = DIRECTIONS[facing];
+
+            point.x = point.x + (command.amount * direction.0);
+            point.y = point.y + (command.amount * direction.1);
+        }
+
+        println!("Part One: {:?}", point.taxicab_distance(Point2D::new(0, 0)));
+    }
+    fn part_two(&self) {
+        let input = fs::read_to_string("inputs/day01_input.txt").unwrap();
+        let commands = input
+            .split(", ")
+            .map(|cmd| Command::parse(cmd))
+            .collect::<Vec<Command>>();
+        let mut facing: usize = 0;
+        let mut point: Point2D = Point2D::new(0, 0);
+        let mut pos_history: HashMap<String, bool> = HashMap::new();
+
+        'outer: for command in commands {
+            update_facing(&mut facing, command.direction);
+            let direction = DIRECTIONS[facing];
+
+            for _ in 0..command.amount {
+                point.x = point.x + direction.0;
+                point.y = point.y + direction.1;
+
+                match pos_history.get(point.hash().as_str()) {
+                    Some(_) => {
+                        break 'outer;
+                    }
+                    None => {
+                        pos_history.insert(String::from(point.hash()), true);
+                        continue;
+                    }
+                }
+            }
+        }
+
+        println!("Part Two: {:?}", point.taxicab_distance(Point2D::new(0, 0)));
+    }
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+const DIRECTIONS: [(i16, i16); 4] = [
     // x, y
     (0, 1),  // N - 0
     (1, 0),  // E - 1
@@ -11,58 +70,52 @@ const DIRECTIONS: [(i8, i8); 4] = [
     (-1, 0), // W - 3
 ];
 
+fn wrap_index(desired: i16, size: i16) -> usize {
+    let u_index = u16::try_from(((desired + size) % size) % size).unwrap();
+
+    return usize::from(u_index);
+}
+
+fn update_facing(was: &mut usize, turn: String) {
+    if turn == "R" {
+        *was = wrap_index(i16::try_from(*was).unwrap() + 1, 4);
+    } else if turn == "L" {
+        *was = wrap_index(i16::try_from(*was).unwrap() - 1, 4);
+    }
+}
+
 #[derive(Debug)]
-struct Point {
-    x: i64,
-    y: i64,
-    facing: usize,
+struct Command {
+    direction: String,
+    amount: i16,
 }
 
-impl<'a> Point {
-    fn exec_command(&mut self, command: &str) {
-        let direction = &command[0..1];
-        let amount = command[1..].parse::<i64>().unwrap();
+impl Command {
+    pub fn parse(input: &str) -> Command {
+        let direction = &input[0..1];
+        let amount = input[1..].parse::<i16>().unwrap();
 
-        let mut new_facing: usize = self.facing;
-        if direction == "R" {
-            new_facing += 1;
-            if new_facing == 4 {
-                new_facing = 0
-            }
-        } else if direction == "L" {
-            match new_facing.checked_sub(1) {
-                Some(val) => new_facing = val,
-                None => new_facing = 3,
-            }
-        }
-
-        self.facing = new_facing;
-        let direction_modifier: (i8, i8) = DIRECTIONS[new_facing];
-        self.x = self.x + (i64::from(direction_modifier.0) * amount);
-        self.y = self.y + (i64::from(direction_modifier.1) * amount);
-    }
-
-    fn taxicab_distance(self, x: i64, y: i64) -> i64 {
-        return (self.x - x).abs() + (self.y - y).abs();
-    }
-}
-
-pub struct Day01 {}
-
-impl Task for Day01 {
-    fn part_one(&self) {
-        let contents = fs::read_to_string("inputs/day01_input.txt").unwrap();
-        let commands: Vec<&str> = contents.split(", ").collect();
-        let mut point = Point {
-            x: 0,
-            y: 0,
-            facing: 0,
+        return Command {
+            direction: String::from(direction),
+            amount: amount,
         };
-
-        for command in commands {
-            point.exec_command(command);
-        }
-        println!("Part One: {:?}", point.taxicab_distance(0, 0));
     }
-    fn part_two(&self) {}
+}
+
+#[derive(Debug)]
+struct Point2D {
+    x: i16,
+    y: i16,
+}
+
+impl<'a> Point2D {
+    pub fn new(x: i16, y: i16) -> Point2D {
+        return Point2D { x: x, y: y };
+    }
+    fn hash(&self) -> String {
+        return format!("{},{}", self.x, self.y);
+    }
+    fn taxicab_distance(&self, other: Point2D) -> i16 {
+        return (self.x - other.x).abs() + (self.y - other.y).abs();
+    }
 }
