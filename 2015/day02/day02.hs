6c4f09d19bf81a91e81ed12df3053c33cd2a216b
Augusto Cesar <augusto.acfs@gmail.com>
2020-12-28 15:27:49 +0100

    2015: Day 02: 🔧 Trying to make it better

--- 2015/day02/day02.hs
+++ 2015/day02/day02.hs
@@ -1,14 +1,12 @@
 import Data.List (sort)
 
 wrappingArea :: [Int] -> Int
-wrappingArea input = do
-  let l = input !! 0
-  let h = input !! 1
-  let w = input !! 2
-  let slack = product $ xSmaller input 2
-
-  -- 2*l*w + 2*w*h + 2*h*l
-  2 * l * w + 2 * w * h + 2 * h * l + slack
+wrappingArea input = 2 * l * w + 2 * w * h + 2 * h * l + slack
+  where
+    l = input !! 0
+    h = input !! 1
+    w = input !! 2
+    slack = product $ xSmaller input 2
 
 ribbonArea :: [Int] -> Int
 ribbonArea input = do
@@ -18,16 +16,24 @@ xSmaller :: [Int] -> Int -> [Int]
 xSmaller l x = do
   take x . sort $ l
 
-split :: [Char] -> [[Char]]
-split str = case break (== 'x') str of
-  (a, 'x' : b) -> a : split b
-  (a, "") -> [a]
+-- For supporting empty list
+tail' :: [a] -> [a]
+tail' (_ : xs) = xs
+tail' [] = []
+
+split :: Char -> [Char] -> [[Char]]
+split _ [] = []
+split d s = x : split d (tail' s')
+  where
+    (x, s') = span (/= d) s
 
 parseString :: String -> [Int]
-parseString str = map read (split str)
+parseString str = map read (split 'x' str)
 
 parseInput :: String -> [[Int]]
 parseInput input = map parseString $ lines input
+
+-- --------------------------------------------------------------------------------------------------------------------
 
 partOne :: String -> IO ()
 partOne input = do
