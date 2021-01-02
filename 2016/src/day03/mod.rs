60982adadc70778793f03d5b9d7bb1ff227915fd
Augusto Cesar <augusto.acfs@gmail.com>
2021-01-02 13:54:43 +0100

    2016: Day 03: Added part two

--- 2016/src/day03/mod.rs
+++ 2016/src/day03/mod.rs
@@ -19,7 +19,7 @@ impl Task for Day03 {
             let b = cap[2].parse::<i16>();
             let c = cap[3].parse::<i16>();
 
-            if is_valid([a.unwrap(), b.unwrap(), c.unwrap()]) {
+            if is_valid(&[a.unwrap(), b.unwrap(), c.unwrap()]) {
                 valid += 1;
             }
         }
@@ -27,13 +27,56 @@ impl Task for Day03 {
         println!("Part One: {}", valid);
     }
     fn part_two(&self) {
-        println!("Part Two");
+        // 0 1 2
+        // 3 4 5
+        // 6 7 8
+        // reset
+        // ...
+
+        let input = self.read_input();
+        let re = Regex::new(r"(\d+)\s+(\d+)\s+(\d+)").unwrap();
+
+        let mut rows: Vec<[i16; 3]> = vec![];
+        for cap in re.captures_iter(&input) {
+            let a = cap[1].parse::<i16>();
+            let b = cap[2].parse::<i16>();
+            let c = cap[3].parse::<i16>();
+            rows.push([a.unwrap(), b.unwrap(), c.unwrap()])
+        }
+
+        let mut valid = 0;
+        let mut pivot = 0;
+        let mut curr_batch = [[0; 3], [0; 3], [0; 3]];
+
+        for i in 0..rows.len() {
+            let row = rows[i];
+            for j in 0..row.len() {
+                let idx = pivot / 3;
+                let triangle = pivot % 3;
+
+                curr_batch[triangle][idx] = row[j];
+                pivot += 1;
+            }
+
+            if pivot == 9 {
+                for triangle in &curr_batch {
+                    if is_valid(triangle) {
+                        valid += 1;
+                    }
+                }
+
+                pivot = 0;
+                curr_batch = [[0; 3], [0; 3], [0; 3]];
+            }
+        }
+
+        println!("Part Two: {}", valid);
     }
 }
 
 // --------------------------------------------------------------------------------------------------------------------
 
-fn is_valid(triangle: [i16; 3]) -> bool {
+fn is_valid(triangle: &[i16; 3]) -> bool {
     let (a, b, c) = (triangle[0], triangle[1], triangle[2]);
     if a + b <= c || a + c <= b || b + c <= a {
         return false;
