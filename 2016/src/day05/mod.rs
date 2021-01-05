d232aa5e1f648bcd53e5a322e7017c59db7ff00c
Augusto Cesar <augusto.acfs@gmail.com>
2021-01-05 21:39:47 +0100

    2016: Day 05: Added part two
    Also small change on part one
    
    Part One: 13.32s
    Part Two: 33.94s

--- 2016/src/day05/mod.rs
+++ 2016/src/day05/mod.rs
@@ -13,19 +13,42 @@ impl Task for Day05 {
         let input = self.read_input();
         let door_id = input.as_str();
         let mut index = 0;
-        let mut password: Vec<char> = vec![];
+        let mut password: [char; 8] = ['-'; 8];
 
-        while password.len() < 8 {
-            let (next, pass_char) = find_next_interesting(door_id, index);
+        for i in 0..8 {
+            let (next, hex) = find_next_interesting(door_id, index);
 
-            password.push(pass_char);
+            password[i] = hex[5];
             index = next + 1;
         }
 
-        println!("Part One: {:?}", String::from_iter(password));
+        println!("Part One: {}", String::from_iter(&password));
     }
     fn part_two(&self) {
-        println!("Part Two");
+        let input = self.read_input();
+        let door_id = input.as_str();
+        let mut index = 0;
+        let mut password: [char; 8] = ['-'; 8];
+
+        while password.iter().any(|x| *x == '-') {
+            let (next, hex) = find_next_interesting(door_id, index);
+
+            index = next + 1;
+            match hex[5].to_digit(10) {
+                Some(val) => {
+                    let pass_idx = val as usize;
+                    if pass_idx > 7 || password[pass_idx] != '-' {
+                        continue;
+                    }
+
+                    password[pass_idx] = hex[6];
+                }
+                None => continue
+            }
+        }
+
+
+        println!("Part Two: {}", String::from_iter(&password));
     }
 }
 
@@ -40,11 +63,11 @@ fn generate_hash_hex(string: &str) -> String {
     return hex::encode(wot);
 }
 
-fn find_next_interesting(id: &str, index: i32) -> (i32, char) {
-    let mut pass_char: char = '.';
+fn find_next_interesting(id: &str, index: i32) -> (i32, Vec<char>) {
+    let mut hex: Vec<char> = vec![];
     let mut new_index: i32 = index;
 
-    while pass_char == '.' {
+    while hex.len() == 0 {
         let mut id_string: String = id.to_string();
         id_string.push_str(new_index.to_string().as_str());
         let hash_hex_chars = generate_hash_hex(id_string.as_str())
@@ -52,11 +75,11 @@ fn find_next_interesting(id: &str, index: i32) -> (i32, char) {
             .collect::<Vec<char>>();
 
         if hash_hex_chars[0..5].iter().all(|x| *x == '0') {
-            pass_char = hash_hex_chars[5];
+            hex = hash_hex_chars;
         }
 
         new_index += 1;
     }
 
-    return (new_index, pass_char);
+    return (new_index, hex);
 }
