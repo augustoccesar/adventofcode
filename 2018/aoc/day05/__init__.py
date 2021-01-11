d77be1def810cbc354c4ae595583f969efe7ee86
Augusto Cesar <augusto.acfs@gmail.com>
2021-01-11 21:02:56 +0100

    2018: Day 05: Added part one

--- 2018/aoc/day05/__init__.py
+++ 2018/aoc/day05/__init__.py
@@ -3,7 +3,36 @@ from aoc.task import Task
 
 class Day05(Task):
     def part_one(self) -> str:
-        return "Not Implemented"
+        data = self.read_input()
+        units = list(data)
+
+        i = 0
+        while True:
+            if i < 0:
+                i = 0
+
+            curr_unit = units[i]
+            next_unit = units[i + 1]
+
+            if is_opposite(curr_unit, next_unit):
+                del units[i:i + 2]
+                i -= 1
+                continue
+
+            i += 1
+
+            # Do this by the end of while
+            if i == len(units) - 1:
+                break
+
+        print(units)
+
+        return str(len(units))
 
     def part_two(self) -> str:
         return "Not Implemented"
+
+
+def is_opposite(left: str, right: str) -> bool:
+    return (left.lower() == right.lower()) \
+           and ((left.isupper() and right.islower()) or (left.islower() and right.isupper()))
