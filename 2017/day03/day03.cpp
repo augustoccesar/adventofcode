7847d92dbd2ab405d5f664f233f85791ec611866
Augusto Cesar <augusto.acfs@gmail.com>
2021-01-15 19:47:35 +0100

    2017: Day 03: Added part two
    Most definitely there are a bunch of possible improvements on this one

--- 2017/day03/day03.cpp
+++ 2017/day03/day03.cpp
@@ -7,31 +7,30 @@
 
 #include "../Task.h"
 
-// Visualization
-// 37  36  35  34  33  32  31
-// 38  17  16  15  14  13  30
-// 39  18   5   4   3  12  29
-// 40  19   6   1   2  11  28
-// 41  20   7   8   9  10  27
-// 42  21  22  23  24  25  26
-// 43  44  45  46  47  48  49...
-
-// 5  4  3
-// 6  1  2
-// 7  8  9
-//
-// (-1, 1) (0, 1) (1, 1)
-// (-1, 0) (0, 0) (1, 0)
-// (-1, -1) (0, -1) (1, -1)
-
-// 1, 9, 25, 49 are perfect squares of odd numbers
-
 using point = std::tuple<int, int>;
 
 class Day03 : public AbstractTask {
  public:
   std::string part_one() override {
-    int square_id = std::stoi(read_input("inputs/day03_input.txt"));
+    // Visualization
+    // 37  36  35  34  33  32  31
+    // 38  17  16  15  14  13  30
+    // 39  18   5   4   3  12  29
+    // 40  19   6   1   2  11  28
+    // 41  20   7   8   9  10  27
+    // 42  21  22  23  24  25  26
+    // 43  44  45  46  47  48  49...
+
+    // 5  4  3
+    // 6  1  2
+    // 7  8  9
+    //
+    // (-1, 1)  (0, 1)  (1, 1)
+    // (-1, 0)  (0, 0)  (1, 0)
+    // (-1, -1) (0, -1) (1, -1)
+
+    // 1, 9, 25, 49 are perfect squares of odd numbers
+    int square_id = std::stoi(input());
     int next_sqrt = sqrt(square_id);
 
     if (next_sqrt % 2 == 0) {
@@ -58,7 +57,88 @@ class Day03 : public AbstractTask {
     return std::to_string(dist);
   }
 
-  std::string part_two() override { return ""; }
+  std::string part_two() override {
+    // Visualization
+    // 147  142  133  122   59
+    // 304    5    4    2   57
+    // 330   10    1    1   54
+    // 351   11   23   25   26
+    // 362  747  806  1587 2467 ...
+
+    // (-2, 2)  (-1, 2)  (0, 2)  (1, 2)  (2, 2)
+    // (-2, 1)  (-1, 1)  (0, 1)  (1, 1)  (2, 1)
+    // (-2, 0)  (-1, 0)  (0, 0)  (1, 0)  (2, 0)
+    // (-2, -1) (-1, -1) (0, -1) (1, -1) (2, -1)
+    // (-2, -2) (-1, -2) (0, -2) (1, -2) (2, -2) ...
+    int looking_for = std::stoi(input());
+    int result;
+
+    std::map<std::string, int> hist;
+    point curr_point = {1, 0};
+    hist[p_key({0, 0})] = 1;
+
+    // Which direction currently moving
+    // +y = 0
+    // -x = 1
+    // -y = 2
+    // +x = 3
+    int moving = 0;
+
+    point next_exit_corner = {1, -1};
+
+    while (true) {
+      point next_point;
+      auto [curr_x, curr_y] = curr_point;
+
+      auto neighbors = neighbors_from_hist(curr_point, hist);
+      int sum = 0;
+      for (auto& neighbor : neighbors) {
+        sum += neighbor.second;
+      }
+
+      if (sum > looking_for) {
+        result = sum;
+        break;
+      }
+
+      hist[p_key(curr_point)] = sum;
+
+      if (abs(curr_x) == abs(curr_y)) {  // Is corner
+        if (moving == 3) {
+          moving = 0;
+        } else {
+          moving++;
+        }
+      }
+
+      if (curr_point == next_exit_corner) {
+        next_point = {curr_x + 1, curr_y};
+        auto [nec_x, nec_y] = next_exit_corner;
+        next_exit_corner = {nec_x + 1, nec_y - 1};
+      } else {
+        switch (moving) {
+          case 0:
+            next_point = {curr_x, curr_y + 1};
+            break;
+          case 1:
+            next_point = {curr_x - 1, curr_y};
+            break;
+          case 2:
+            next_point = {curr_x, curr_y - 1};
+            break;
+          case 3:
+            next_point = {curr_x + 1, curr_y};
+            break;
+          default:
+            throw "Invalid moving";
+        }
+      }
+
+      curr_point = next_point;
+    }
+
+    return std::to_string(result);
+  }
 
  private:
   std::string input() { return read_input("inputs/day03_input.txt"); }
@@ -128,5 +208,32 @@ class Day03 : public AbstractTask {
     auto [a_x, a_y] = a;
     auto [b_x, b_y] = b;
     return abs(a_x - b_x) + abs(a_y - b_y);
+  }
+
+  std::string p_key(point p) {
+    return std::to_string(std::get<0>(p)) + "," +
+           std::to_string(std::get<1>(p));
+  }
+
+  std::vector<std::pair<point, int>> neighbors_from_hist(
+      point p, std::map<std::string, int> hist) {
+    std::vector<std::pair<point, int>> list;
+    std::vector<std::pair<int, int>> modifiers{
+        std::pair(-1, 1), std::pair(0, 1),   std::pair(1, 1),  std::pair(-1, 0),
+        std::pair(1, 0),  std::pair(-1, -1), std::pair(0, -1), std::pair(1, -1),
+    };
+
+    for (auto& mod : modifiers) {
+      auto [mod_x, mod_y] = mod;
+      auto [x, y] = p;
+      point new_point = {x + mod_x, y + mod_y};
+
+      auto found = hist.find(p_key(new_point));
+      if (found != hist.end()) {
+        list.push_back(std::pair(new_point, found->second));
+      }
+    }
+
+    return list;
   }
 };
