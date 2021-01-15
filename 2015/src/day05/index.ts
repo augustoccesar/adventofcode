b22b0ee89590fd4670dabd57e2f955a6e1cd6906
Augusto Cesar <augusto.acfs@gmail.com>
2021-01-15 21:01:06 +0100

    2015: Day 05: Added part two

--- 2015/src/day05/index.ts
+++ 2015/src/day05/index.ts
@@ -16,12 +16,12 @@ export class Day05 extends Task {
             for (let i = 0; i < str.split("").length; i++) {
                 if (vowels.includes(str[i])) { vowelsCount++; }
 
-                if(i != 0 && !containsConsecutive) {
-                    if(str[i] == str[i-1]) { containsConsecutive = true; }
+                if (i != 0 && !containsConsecutive) {
+                    if (str[i] == str[i - 1]) { containsConsecutive = true; }
                 }
             }
 
-            if(vowelsCount >= 3 && containsConsecutive) {
+            if (vowelsCount >= 3 && containsConsecutive) {
                 niceWords++;
             }
         }
@@ -30,6 +30,29 @@ export class Day05 extends Task {
     }
 
     partTwo(): String {
-        return "";
+        let niceWords: number = 0;
+
+        for (let str of this.readInput().split("\n")) {
+            let check_1 = false;
+            let check_2 = false;
+            for (let i = 0; i < str.split("").length; i++) {
+                if (i == 0) { continue; }
+
+                const repeat = str.match(new RegExp(str.substr(i - 1, 2), "g"));
+                if (repeat != null && repeat.length > 1) {
+                    check_1 = true;
+                }
+
+                if (i > 1 && str[i] == str[i - 2]) {
+                    check_2 = true;
+                }
+            }
+
+            if (check_1 && check_2) {
+                niceWords++;
+            }
+        }
+
+        return niceWords.toString();
     }
 }
