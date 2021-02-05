2ed3d0b7b1ddf56fed09c4533490ca9a4f045b42
Augusto Cesar <augusto.acfs@gmail.com>
2021-02-05 19:04:22 +0100

    2019: Day 09: Added part one

--- 2019/src/main/java/com/augustoccesar/adventofcode/day09/Day09.java
+++ 2019/src/main/java/com/augustoccesar/adventofcode/day09/Day09.java
@@ -1,13 +1,22 @@
 package com.augustoccesar.adventofcode.day09;
 
 import com.augustoccesar.adventofcode.Task;
+import com.augustoccesar.adventofcode.day05.IntComputer;
 import java.io.IOException;
 
 public class Day09 extends Task {
 
   @Override
   public String partOne() throws IOException {
-    return "-";
+    String program = this.readInput();
+    IntComputer computer = IntComputer.load(program);
+    computer.addInput(1);
+
+    while (!computer.isHalted()) {
+      computer.run();
+    }
+
+    return String.valueOf(computer.lastOutput());
   }
 
   @Override
