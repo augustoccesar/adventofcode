f5e0985866f655dcad8fb1a696afd13973bd4219
Augusto Cesar <augusto.acfs@gmail.com>
2021-02-05 13:39:28 +0100

    2019: Day 09: Registered

---
+++ 2019/src/main/java/com/augustoccesar/adventofcode/day09/Day09.java
@@ -0,0 +1,17 @@
+package com.augustoccesar.adventofcode.day09;
+
+import com.augustoccesar.adventofcode.Task;
+import java.io.IOException;
+
+public class Day09 extends Task {
+
+  @Override
+  public String partOne() throws IOException {
+    return "-";
+  }
+
+  @Override
+  public String partTwo() throws IOException {
+    return "-";
+  }
+}
