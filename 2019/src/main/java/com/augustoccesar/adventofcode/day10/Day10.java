71efa6edb16e4fdaf1299fec4af0785ae9147f43
Augusto Cesar <augusto.acfs@gmail.com>
2021-02-28 14:32:46 +0100

    2019: Day 10: Added part one

--- 2019/src/main/java/com/augustoccesar/adventofcode/day10/Day10.java
+++ 2019/src/main/java/com/augustoccesar/adventofcode/day10/Day10.java
@@ -4,12 +4,33 @@ import com.augustoccesar.adventofcode.Task;
 import com.augustoccesar.adventofcode.utils.Point2D;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Day10 extends Task {
   @Override
   public String partOne() throws IOException {
-    return "-";
+    ArrayList<Point2D> asteroids = this.getAsteroids(this.readInput());
+    int maxVisible = 0;
+
+    for (final Point2D base : asteroids) {
+      HashMap<Double, Set<Point2D>> visible = new HashMap<>();
+      for (final Point2D asteroid : asteroids) {
+        double angle = getAngle(base, asteroid);
+        if (!visible.containsKey(angle)) {
+          visible.put(angle, new HashSet<>());
+        }
+
+        visible.get(angle).add(asteroid);
+      }
+      if (visible.size() > maxVisible) {
+        maxVisible = visible.size();
+      }
+    }
+
+    return String.valueOf(maxVisible);
   }
 
   @Override
@@ -17,11 +38,12 @@ public class Day10 extends Task {
     return "-";
   }
 
-  private ArrayList<Point2D> getAsteroids(final String map) {
+  private ArrayList<Point2D> getAsteroids(final String inputMap) {
     AtomicInteger currY = new AtomicInteger();
     ArrayList<Point2D> asteroids = new ArrayList<>();
 
-    map.lines()
+    inputMap
+        .lines()
         .forEach(
             line -> {
               String[] objects = line.split("");
@@ -35,5 +57,15 @@ public class Day10 extends Task {
             });
 
     return asteroids;
+  }
+
+  private double getAngle(final Point2D pointA, final Point2D pointB) {
+    double angle =
+        Math.atan2(pointB.getY() - pointA.getY(), pointB.getX() - pointA.getX()) * 180 / Math.PI;
+    if (angle < 0) {
+      return 360 - angle;
+    }
+
+    return angle;
   }
 }
