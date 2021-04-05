7be30f57946e6679f332e512906f0c032d81fcc7
Augusto Cesar <augusto.acfs@gmail.com>
2021-04-05 19:22:58 +0200

    2016: Day 06: Added part two

--- 2016/src/day06/mod.rs
+++ 2016/src/day06/mod.rs
@@ -10,45 +10,67 @@ impl Task for Day06 {
     }
 
     fn part_one(&self) {
-        let mut message_size: i8 = -1;
-        let mut mapped_data: HashMap<usize, Vec<char>> = HashMap::new();
-        let data = self.read_input();
+        let result = decode_message(self.read_input(), DECODE_MODE_MAX);
 
-        data.lines().for_each(|line| {
-            let chars: Vec<char> = line.chars().collect();
-            if message_size == -1 {
-                message_size = chars.len() as i8;
-            }
-            for i in 0..chars.len() {
-                let entry: &mut Vec<char> = match mapped_data.entry(i) {
-                    Entry::Occupied(o) => o.into_mut(),
-                    Entry::Vacant(v) => v.insert(Vec::new()),
-                };
-
-                entry.push(chars[i]);
-            }
-        });
-
-        let mut result: Vec<char> = Vec::new();
-        for i in 0..message_size {
-            let mut occurrences: HashMap<char, i8> = HashMap::new();
-            let chars: &Vec<char> = mapped_data.get(&(i as usize)).unwrap();
-            for item in chars {
-                *occurrences.entry(*item).or_default() += 1;
-            }
-
-            let max = occurrences
-                .into_iter()
-                .max_by_key(|(_, v)| *v)
-                .map(|(k, _)| k);
-            result.push(max.unwrap());
-        }
-
-        println!("Part One: {}", result.into_iter().collect::<String>());
+        println!("Part One: {}", result);
     }
     fn part_two(&self) {
-        println!("Part Two: {}", "-");
+        let result = decode_message(self.read_input(), DECODE_MODE_MIN);
+
+        println!("Part Two: {}", result);
     }
 }
 
 // --------------------------------------------------------------------------------------------------------------------
+
+type DecodeMode = i8;
+const DECODE_MODE_MAX: DecodeMode = 1;
+const DECODE_MODE_MIN: DecodeMode = 2;
+
+fn decode_message(data: String, decode_mode: DecodeMode) -> String {
+    let mut message_size: i8 = -1;
+    let mut mapped_data: HashMap<usize, Vec<char>> = HashMap::new();
+
+    data.lines().for_each(|line| {
+        let chars: Vec<char> = line.chars().collect();
+        if message_size == -1 {
+            message_size = chars.len() as i8;
+        }
+        for i in 0..chars.len() {
+            let entry: &mut Vec<char> = match mapped_data.entry(i) {
+                Entry::Occupied(o) => o.into_mut(),
+                Entry::Vacant(v) => v.insert(Vec::new()),
+            };
+
+            entry.push(chars[i]);
+        }
+    });
+
+    let mut result: Vec<char> = Vec::new();
+    for i in 0..message_size {
+        let mut occurrences: HashMap<char, i8> = HashMap::new();
+        let chars: &Vec<char> = mapped_data.get(&(i as usize)).unwrap();
+        for item in chars {
+            *occurrences.entry(*item).or_default() += 1;
+        }
+
+        let mut max: char = '-';
+        if decode_mode == DECODE_MODE_MAX {
+            max = occurrences
+                .into_iter()
+                .max_by_key(|(_, v)| *v)
+                .map(|(k, _)| k)
+                .unwrap();
+        } else if decode_mode == DECODE_MODE_MIN {
+            max = occurrences
+                .into_iter()
+                .min_by_key(|(_, v)| *v)
+                .map(|(k, _)| k)
+                .unwrap();
+        }
+
+        result.push(max);
+    }
+
+    return result.into_iter().collect::<String>();
+}
