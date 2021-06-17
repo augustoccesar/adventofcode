c95605b8900b3a521780eb85ffe098aed8ce7e0e
Augusto Cesar <augusto.freitas-e-silva@izettle.com>
2021-06-17 17:19:09 +0200

    2019: Day 12: 🔧 Implement orbit

--- 2019/src/main/java/com/augustoccesar/adventofcode/day12/Day12.java
+++ 2019/src/main/java/com/augustoccesar/adventofcode/day12/Day12.java
@@ -3,6 +3,7 @@ package com.augustoccesar.adventofcode.day12;
 import com.augustoccesar.adventofcode.Task;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,21 +16,13 @@ public class Day12 extends Task {
   @Override
   public String partOne() throws IOException {
     List<Moon> moons = parseInput(this.readInput());
+    Orbit orbit = Orbit.with(moons);
 
     for (int i = 0; i < 1000; i++) {
-      for (final Moon moon : moons) {
-        moon.applyGravity(moons);
-      }
-
-      for (final Moon moon : moons) {
-        moon.applyVelocity();
-      }
+      orbit.tick();
     }
 
-    int totalEnergy = moons.stream()
-        .map(Moon::totalEnergy)
-        .reduce(Integer::sum)
-        .orElseThrow(() -> new RuntimeException("Failed to sum total energy of the moons"));
+    int totalEnergy = orbit.energy();
 
     return String.valueOf(totalEnergy);
   }
