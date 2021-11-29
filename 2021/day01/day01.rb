b51cd713aab4bc93b9b2d760a591cf2509d15027
Augusto César <augusto@augustocesar.se>
2021-11-29 11:47:07 -0300

    2021(misc): prepare project

---
+++ 2021/day01/day01.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative "../task"
+
+class Day01
+  include Task
+
+  def part_one
+    data = read_input
+    raise "Wot" unless data == "TBA"
+
+    "-"
+  end
+
+  def part_two
+    data = read_input
+    raise "Wot" unless data == "TBA"
+
+    "-"
+  end
+end
