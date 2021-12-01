73a175784bad114e737f8a596056b298a3224b34
Augusto César <augusto@augustocesar.se>
2021-12-01 10:25:34 -0300

    2021(1): Solved part one

--- 2021/day01/day01.rb
+++ 2021/day01/day01.rb
@@ -6,16 +6,17 @@ class Day01
   include Task
 
   def part_one
-    data = read_input
-    raise "Wot" unless data == "TBA"
+    data = read_input.lines.map(&:to_i)
 
-    "-"
+    increased = 0
+    (1..data.length - 1).each do |i|
+      increased += 1 if data[i] > data[i - 1]
+    end
+
+    increased.to_s
   end
 
   def part_two
-    data = read_input
-    raise "Wot" unless data == "TBA"
-
     "-"
   end
 end
