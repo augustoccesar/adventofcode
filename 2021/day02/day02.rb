0843d2355575abc97ad4f4f2cf8cbb4171bd52dc
Augusto Cesar <augusto@augustocesar.se>
2021-12-13 13:37:20 +0100

    2021(2): refactor part one considering part two

--- 2021/day02/day02.rb
+++ 2021/day02/day02.rb
@@ -5,21 +5,21 @@ require_relative "../task"
 class Day02
   include Task
 
-  ACTION_MAP = {
-    forward: [1, 0],
-    down: [0, 1],
-    up: [0, -1]
-  }.freeze
-
   def part_one
+    action_map = {
+      forward: [1, 0],
+      down: [0, 1],
+      up: [0, -1]
+    }.freeze
+
     xy = [0, 0]
 
     read_input.lines.map(&:split).each do |command_data|
-      action = ACTION_MAP[command_data[0].to_sym]
+      action = action_map[command_data[0].to_sym]
       num = command_data[1].to_i
 
-      xy[0] = xy[0] + (action[0] * num)
-      xy[1] = xy[1] + (action[1] * num)
+      xy[0] += (action[0] * num)
+      xy[1] += (action[1] * num)
     end
 
     xy.reduce(&:*).to_s
