0b1d023fcce7b23d55d27e0fc712ae36363f8fe4
Augusto Cesar <augusto@augustocesar.se>
2021-12-14 19:30:56 +0100

    2021(4): solved part two

--- 2021/day04/day04.rb
+++ 2021/day04/day04.rb
@@ -10,13 +10,40 @@ class Day04
 
   def part_one
     numbers, boards = parse_input(read_input)
+    result = run(numbers, boards, to: :first_winner)
+
+    result.to_s
+  end
+
+  def part_two
+    numbers, boards = parse_input(read_input)
+    result = run(numbers, boards, to: :last_winner)
+
+    result.to_s
+  end
+
+  private
+
+  def run(numbers, boards, to:)
     marked_track = Array.new(boards.size) { [Array.new(5, 0), Array.new(5, 0)] }
     marked_track_nums = Array.new(boards.size) { [] }
+    winners = []
+    limit = nil
 
-    winner = nil
-    while winner.nil?
+    case to
+    when :first_winner
+      limit = 1
+    when :last_winner
+      limit = boards.size
+    else
+      raise ArgumentError("Invalid 'to' limit. Must be one of: [:first_winner, :last_winner]")
+    end
+
+    while winners.size < limit
       num = numbers.shift
       boards.each_with_index do |board, i|
+        next if winners.include?(i)
+
         pos = board[num]
         next if pos.nil?
 
@@ -24,21 +51,15 @@ class Day04
         marked_track[i][1][pos[1]] += 1
         marked_track_nums[i] << num
 
-        winner = i if marked_track[i][0][pos[0]] == 5 || marked_track[i][1][pos[1]] == 5
+        winners << i if marked_track[i][0][pos[0]] == 5 || marked_track[i][1][pos[1]] == 5
       end
     end
 
-    unmarked_sum = (boards[winner].keys.map(&:to_i) - marked_track_nums[winner].map(&:to_i)).reduce(&:+)
-    last_marked = marked_track_nums[winner][-1].to_i
+    unmarked_sum = (boards[winners[-1]].keys.map(&:to_i) - marked_track_nums[winners[-1]].map(&:to_i)).reduce(&:+)
+    last_marked = marked_track_nums[winners[-1]][-1].to_i
 
     (unmarked_sum * last_marked).to_s
   end
-
-  def part_two
-    "-"
-  end
-
-  private
 
   def parse_input(input)
     data = input.split("\n\n")
