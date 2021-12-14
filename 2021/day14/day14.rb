2f30020c1de9d15aa510d182c4a316ed96a018dc
Augusto Cesar <augusto@augustocesar.se>
2021-12-14 19:30:09 +0100

    2021(14): solved part two

--- 2021/day14/day14.rb
+++ 2021/day14/day14.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# TODO(augustoccesar)[2021-12-14]: This can very probably be written better. Take a look at it again.
+# TODO(augustoccesar)[2021-12-14]: Explain what am I doing with some comments (unless TODO above makes it simpler)
+
 require_relative "../task"
 
 class Day14
@@ -8,41 +11,50 @@ class Day14
   def part_one
     template, pair_rules = parse_input(read_input)
 
-    10.times do
-      template = iter(template, pair_rules)
-    end
-
-    registry = {}
-    template.chars.each do |c|
-      registry[c] = 0 unless registry.key?(c)
-
-      registry[c] += 1
-    end
-
-    (registry.values.max - registry.values.min).to_s
+    run(template, pair_rules, 10).to_s
   end
 
   def part_two
-    "-"
+    template, pair_rules = parse_input(read_input)
+
+    run(template, pair_rules, 40).to_s
   end
 
   private
 
-  def iter(template, pair_rules)
-    template_chars = template.chars
+  def run(template, pair_rules, num)
+    chars = template.chars
+    pairs = {}
+    (0..chars.size - 2).each do |i|
+      pair = chars[i] + chars[i + 1]
+      pairs[pair] = 0 unless pairs.key?(pair)
 
-    insertions = []
-    (0..template_chars.size - 2).each do |i|
-      pair = [template_chars[i], template_chars[i + 1]].reduce(&:+)
-      insertions << [i + 1 + insertions.size, pair_rules[pair]]
+      pairs[pair] += 1
     end
 
-    new_template = template.chars
-    insertions.each do |ins|
-      new_template.insert(ins[0], ins[1])
+    num.times do
+      new_pairs = {}
+      pairs.each do |pair, count|
+        res = pair_rules[pair]
+
+        [pair[0] + res, res + pair[1]].each do |new_pair|
+          new_pairs[new_pair] = 0 unless new_pairs.key?(new_pair)
+
+          new_pairs[new_pair] += count
+        end
+      end
+
+      pairs = new_pairs
     end
 
-    new_template.reduce(&:+)
+    occurrences = {}
+    pairs.each do |k, v|
+      occurrences[k[0]] = occurrences.key?(k[0]) ? occurrences[k[0]] + v : v
+      occurrences[k[1]] = occurrences.key?(k[1]) ? occurrences[k[1]] + v : v
+    end
+    occurrences.each { |k, v| occurrences[k] = (v / 2.0).ceil.to_i }
+
+    occurrences.values.max - occurrences.values.min
   end
 
   def parse_input(input)
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 | Language | Status | Week 1 | Week 2 | Week 3 | Week 4 | Week 5 |
 | :-- | :-: | :-: | :-: | :-: |  :-: |  :-: | 
-| [2021 - Ruby 3](2021/README.md) | | ✅✅✅🟡❌ | ❌❌❌❌❌ | ❌❌❌🟡❌ | ❌❌❌❌❌ | ❌❌❌❌❌ |
+| [2021 - Ruby 3](2021/README.md) | | ✅✅✅🟡❌ | ❌❌❌❌❌ | ❌❌❌✅❌ | ❌❌❌❌❌ | ❌❌❌❌❌ |
 | [2020 - Golang 1.15](2020/README.md) | [![2020](https://github.com/augustoccesar/adventofcode/workflows/2020/badge.svg)](https://github.com/augustoccesar/adventofcode/actions?query=workflow%3A2020) | ✅✅✅✅✅| ✅✅✅✅✅ | ✅✅✅✅✅ | ✅✅✅✅✅ | ✅✅✅✅✅ |
 | [2019 - Java 16](2019/README.md) | [![2019](https://github.com/augustoccesar/adventofcode/workflows/2019/badge.svg)](https://github.com/augustoccesar/adventofcode/actions?query=workflow%3A2019) | ✅✅✅✅✅ | ✅✅✅✅✅ | ✅🟡✅✅❌ | ❌❌❌❌❌ | ❌❌❌❌❌ |
 | [2018 - Python 3.8](2018/README.md) | | ✅✅✅✅✅ | ✅🟡❌❌❌ | ❌❌❌❌❌ | ❌❌❌❌❌ | ❌❌❌❌❌ |
