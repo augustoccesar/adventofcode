0c877940c800e3c596660eac0a90d23d299d78f7
Augusto César <augusto@augustocesar.se>
2022-06-14 18:39:15 +0200

    2021(10): solved part two

--- 2021/day10/day10.rb
+++ 2021/day10/day10.rb
@@ -9,11 +9,18 @@ DELIMS = {
   "<" => ">"
 }.freeze
 
-ERROR_POINTS = {
+CORRUPTED_ERROR_POINTS = {
   ")" => 3,
   "]" => 57,
   "}" => 1197,
   ">" => 25_137
+}.freeze
+
+INCOMPLETE_ERROR_POINTS = {
+  ")" => 1,
+  "]" => 2,
+  "}" => 3,
+  ">" => 4
 }.freeze
 
 class Day10
@@ -40,10 +47,37 @@ class Day10
       end
     end
 
-    errors.map { |item| ERROR_POINTS[item] }.reduce(&:+).to_s
+    errors.map { |item| CORRUPTED_ERROR_POINTS[item] }.reduce(&:+).to_s
   end
 
   def part_two
-    "-"
+    scores = []
+
+    read_input.lines.each do |line|
+      corrupted = false
+      delim_stack = []
+
+      line.chars.each do |char|
+        if DELIMS.keys.include?(char)
+          delim_stack << char
+        elsif DELIMS.values.include?(char)
+          if char == DELIMS[delim_stack.last]
+            delim_stack.pop
+            next
+          end
+
+          corrupted = true
+          break
+        end
+      end
+
+      next if corrupted
+
+      scores << delim_stack.reverse.inject(0) do |total, n|
+        (total * 5) + INCOMPLETE_ERROR_POINTS[DELIMS[n]]
+      end
+    end
+
+    scores.sort[(scores.size - 1) / 2].to_s
   end
 end
