e9e9326cb1424d28dddaaa047f9191ae9c37a6db
Augusto César <augusto@augustocesar.se>
2022-07-29 13:00:40 +0200

    2017(7): 🧹 cleaning up some code
    Next step is trying to improve `weighting` method

--- 2017/day07/day07.cpp
+++ 2017/day07/day07.cpp
@@ -1,5 +1,5 @@
-#include <map>
 #include <iostream>
+#include <map>
 #include <regex>
 
 #include "../String.h"
@@ -11,79 +11,26 @@ struct Disc {
   std::vector<std::string> child_discs_names;
 };
 
+struct ParsedInput {
+  std::map<std::string, Disc> discs_map;
+  Disc root;
+};
+
 class Day07 : public AbstractTask {
  public:
   std::string part_one() override {
     std::string input = read_input("inputs/day07_input.txt");
+    ParsedInput parsed_input = parse_input(input);
 
-    std::vector<std::string> root_names;
-    std::map<std::string, bool> connected_to_root_names;
-
-    for (auto line : split(input, '\n')) {
-      std::regex rgx("(\\w+)\\s\\((\\d+)\\)(?:\\s->\\s(.+))?");
-      std::smatch matches;
-
-      if (std::regex_search(line, matches, rgx)) {
-        std::string programs_raw = matches[3];
-        if (programs_raw != "") {
-          programs_raw.erase(
-              std::remove(programs_raw.begin(), programs_raw.end(), ' '),
-              programs_raw.end());
-          std::vector<std::string> nested_programs = split(programs_raw, ',');
-
-          root_names.push_back(matches[1]);
-          for (auto nested_program : nested_programs) {
-            connected_to_root_names[nested_program] = true;
-          }
-        }
-      }
-    }
-
-    for (auto root : root_names) {
-      bool found = connected_to_root_names[root];
-      if (!found) {
-        return root;
-      }
-    }
-
-    throw "unreachable";
+    return parsed_input.root.name;
   }
 
   std::string part_two() override {
     std::string input = read_input("inputs/day07_input.txt");
-    std::map<std::string, Disc> discs_map;
-    std::map<std::string, std::tuple<bool, bool>> child_tracking; 
-
-    for (auto line : split(input, '\n')) {
-      Disc disc = parse_line(line);
-      discs_map[disc.name] = disc;
-
-      bool has_children = disc.child_discs_names.size() > 0;
-      child_tracking[disc.name] = std::make_tuple(has_children, false);
-    }
-
-    for (auto &[disc_name, disc] : discs_map) {
-      if(disc.child_discs_names.size() > 0) {
-        for (auto child_name : disc.child_discs_names) {
-          Disc* child_disc = &discs_map[child_name];
-
-          auto a = &child_tracking[child_disc->name];
-          std::get<1>(*a) = true;
-        }
-      }
-    }
-
-    Disc *root;
-    for (auto &[disc_name, disc] : discs_map) {
-      std::pair<bool, bool> tracking = child_tracking[disc_name];
-      if (std::get<0>(tracking) && !std::get<1>(tracking)) {
-        root = &disc;
-        break;
-      }
-    }
+    ParsedInput parsed_input = parse_input(input);
 
     int response = -1;
-    weighting(root, discs_map, &response);
+    weighting(&parsed_input.root, parsed_input.discs_map, &response);
 
     return std::to_string(response);
   }
@@ -103,17 +50,54 @@ class Day07 : public AbstractTask {
         child_discs_names = split(nested_discs, ',');
       }
 
-      return {
-        .name = matches[1],
-        .weight = stoi(matches[2]),
-        .child_discs_names = child_discs_names
-      };
+      return {.name = matches[1],
+              .weight = stoi(matches[2]),
+              .child_discs_names = child_discs_names};
     }
 
     throw "invalid line";
   }
 
-  int weighting(Disc* base, std::map<std::string, Disc> discs_map, int* response) {
+  ParsedInput parse_input(std::string input) {
+    std::map<std::string, Disc> discs_map;
+    std::map<std::string, std::tuple<bool, bool>> child_tracking;
+
+    for (auto line : split(input, '\n')) {
+      Disc disc = parse_line(line);
+      discs_map[disc.name] = disc;
+
+      bool has_children = disc.child_discs_names.size() > 0;
+      child_tracking[disc.name] = std::make_tuple(has_children, false);
+    }
+
+    for (auto &[disc_name, disc] : discs_map) {
+      if (disc.child_discs_names.size() > 0) {
+        for (auto child_name : disc.child_discs_names) {
+          Disc *child_disc = &discs_map[child_name];
+
+          auto a = &child_tracking[child_disc->name];
+          std::get<1>(*a) = true;
+        }
+      }
+    }
+
+    Disc root;
+    for (auto &[disc_name, disc] : discs_map) {
+      std::pair<bool, bool> tracking = child_tracking[disc_name];
+      if (std::get<0>(tracking) && !std::get<1>(tracking)) {
+        root = disc;
+        break;
+      }
+    }
+
+    return {
+        .discs_map = discs_map,
+        .root = root,
+    };
+  }
+
+  int weighting(Disc *base, std::map<std::string, Disc> discs_map,
+                int *response) {
     if (base->child_discs_names.size() == 0) {
       return base->weight;
     }
@@ -135,10 +119,10 @@ class Day07 : public AbstractTask {
       Disc out_of_weight;
       int wrong_weight;
 
-      for(auto [weight, occurrences] : occurrences) {
-        if(occurrences == 1) {
+      for (auto [weight, occurrences] : occurrences) {
+        if (occurrences == 1) {
           for (int i = 0; i < child_weights.size(); i++) {
-            if(child_weights[i] == weight) {
+            if (child_weights[i] == weight) {
               out_of_weight = discs_map[base->child_discs_names[i]];
               wrong_weight = weight;
               break;
