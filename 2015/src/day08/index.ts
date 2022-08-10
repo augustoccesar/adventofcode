c2cdeaf46548d75e4669ce555f4acc96229481e0
Augusto Silva <augusto.silva@mentimeter.com>
2022-08-10 16:09:04 +0200

    2015: remove duplicate code

--- 2015/src/day08/index.ts
+++ 2015/src/day08/index.ts
@@ -3,32 +3,31 @@ import { Task } from '../Task';
 export class Day08 extends Task {
     partOne(): string {
         const data = this.readInput().split("\n");
-        let codeChars = 0;
-        let memoryChars = 0;
+        const result = calculate(data);
 
-        data.forEach(item => {
-            codeChars += item.length;
-            memoryChars += eval(item).length;
-        });
-
-        return (codeChars - memoryChars).toString();
+        return result.toString();
     }
 
     partTwo(): string {
-        const data = this.readInput().split("\n");
-        let codeChars = 0;
-        let memoryChars = 0;
+        const data = this.readInput().split("\n").map(item => encode(item));
+        const result = calculate(data);
 
-        data.forEach(item => {
-            const encodedItem = encode(item)
-            codeChars += encodedItem.length;
-            memoryChars += eval(encodedItem).length;
-        });
-
-        return (codeChars - memoryChars).toString();
+        return result.toString();
     }
 }
 
 function encode(str: string): string {
     return "\"" + str.replace(/\\/g, "\\\\").replace(/\"/g, "\\\"") + "\""
+}
+
+function calculate(data: string[]): number {
+    let codeChars = 0;
+    let memoryChars = 0;
+
+    data.forEach(item => {
+        codeChars += item.length;
+        memoryChars += eval(item).length;
+    });
+
+    return (codeChars - memoryChars);
 }
