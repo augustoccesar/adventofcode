1aecb8af7819468a3b092928fe9f4cc876158309
Augusto Silva <augusto.silva@mentimeter.com>
2022-08-16 22:41:55 +0200

    2010(10): remove duplicated code

--- 2017/day10/day10.cpp
+++ 2017/day10/day10.cpp
@@ -6,16 +6,51 @@
 class Day10 : public AbstractTask {
  public:
   std::string part_one() override {
-    int circleSize = 256;
     std::vector<int> lengths = parseInput(read_input("inputs/day10_input.txt"));
 
+    int circleSize = 256;
     int circle[circleSize];
-    for (int i = 0; i < circleSize; i++) {
-      circle[i] = i;
-    }
+    initCircle(circle, circleSize);
 
     int position = 0;
     int skipSize = 0;
+    executeLenghts(lengths, circle, circleSize, position, skipSize);
+
+    return std::to_string(circle[0] * circle[1]);
+  }
+
+  std::string part_two() override {
+    std::vector<int> lengths = parseInputBytes(read_input("inputs/day10_input.txt"));
+
+    int circleSize = 256;
+    int circle[circleSize];
+    initCircle(circle, circleSize);
+
+    int position = 0;
+    int skipSize = 0;
+    for (int i = 0; i < 64; i++) {
+      executeLenghts(lengths, circle, circleSize, position, skipSize);
+    }
+
+    int blocks[16];
+    fillBlocks(blocks, circle);
+
+    std::string res = "";
+    for (auto block : blocks) {
+      res += intToHex(block);
+    }
+
+    return res;
+  }
+
+ private:
+  void initCircle(int circle[], int size) {
+    for (int i = 0; i < size; i++) {
+      circle[i] = i;
+    }
+  }
+
+  void executeLenghts(std::vector<int> lengths, int circle[], int circleSize, int& position, int& skipSize) {
     for (auto length : lengths) {
       int pivotL = position;
       int pivotR = (position + length - 1);
@@ -31,52 +66,9 @@ class Day10 : public AbstractTask {
       position = (position + length + skipSize) % circleSize;
       skipSize++;
     }
-
-    return std::to_string(circle[0] * circle[1]);
   }
 
-  std::string part_two() override {
-    std::string input = read_input("inputs/day10_input.txt");
-    std::vector<int> lengths;
-    int extraLengths[] = {17, 31, 73, 47, 23};
-
-    for (int i = 0; i < input.size(); i++) {
-      lengths.push_back(int(input[i]));
-    }
-
-    for (int el : extraLengths) {
-      lengths.push_back(el);
-    }
-
-    // ---
-
-    int circleSize = 256;
-    int circle[circleSize];
-    for (int i = 0; i < circleSize; i++) {
-      circle[i] = i;
-    }
-
-    int position = 0;
-    int skipSize = 0;
-    for (int i = 0; i < 64; i++) {
-      for (auto length : lengths) {
-        int pivotL = position;
-        int pivotR = (position + length - 1);
-        while (pivotL < pivotR) {
-          int tmp = circle[pivotL % circleSize];
-          circle[pivotL % circleSize] = circle[pivotR % circleSize];
-          circle[pivotR % circleSize] = tmp;
-
-          pivotL++;
-          pivotR--;
-        }
-
-        position = (position + length + skipSize) % circleSize;
-        skipSize++;
-      }
-    }
-
-    int blocks[16];
+  void fillBlocks(int blocks[], int circle[]) {
     for (int i = 0; i < 16; i++) {
       int blockStartIdx = i * 16;
       int blockEndIdx = blockStartIdx + 16;
@@ -88,23 +80,28 @@ class Day10 : public AbstractTask {
 
       blocks[i] = block;
     }
-
-    std::string res = "";
-    for (auto block : blocks) {
-      auto hex = intToHex(block);
-      printf("%d -> %s\n", block, hex.c_str());
-      res += hex;
-    }
-
-    return res;
   }
 
- private:
   std::vector<int> parseInput(const std::string& input) {
     std::vector<int> lengths;
 
     for (auto lengthStr : split(input, ',')) {
       lengths.push_back(std::stoi(lengthStr));
+    }
+
+    return lengths;
+  }
+
+  std::vector<int> parseInputBytes(const std::string& input) {
+    std::vector<int> lengths;
+    int extraLengths[] = {17, 31, 73, 47, 23};
+
+    for (int i = 0; i < input.size(); i++) {
+      lengths.push_back(int(input[i]));
+    }
+
+    for (int el : extraLengths) {
+      lengths.push_back(el);
     }
 
     return lengths;
