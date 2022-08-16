94e431e19bdd9175102fa96b254a9e805aa26187
Augusto Silva <augusto.silva@mentimeter.com>
2022-08-16 22:15:28 +0200

    2017(10): solved part two

--- 2017/day10/day10.cpp
+++ 2017/day10/day10.cpp
@@ -35,7 +35,69 @@ class Day10 : public AbstractTask {
     return std::to_string(circle[0] * circle[1]);
   }
 
-  std::string part_two() override { return "-"; }
+  std::string part_two() override {
+    std::string input = read_input("inputs/day10_input.txt");
+    std::vector<int> lengths;
+    int extraLengths[] = {17, 31, 73, 47, 23};
+
+    for (int i = 0; i < input.size(); i++) {
+      lengths.push_back(int(input[i]));
+    }
+
+    for (int el : extraLengths) {
+      lengths.push_back(el);
+    }
+
+    // ---
+
+    int circleSize = 256;
+    int circle[circleSize];
+    for (int i = 0; i < circleSize; i++) {
+      circle[i] = i;
+    }
+
+    int position = 0;
+    int skipSize = 0;
+    for (int i = 0; i < 64; i++) {
+      for (auto length : lengths) {
+        int pivotL = position;
+        int pivotR = (position + length - 1);
+        while (pivotL < pivotR) {
+          int tmp = circle[pivotL % circleSize];
+          circle[pivotL % circleSize] = circle[pivotR % circleSize];
+          circle[pivotR % circleSize] = tmp;
+
+          pivotL++;
+          pivotR--;
+        }
+
+        position = (position + length + skipSize) % circleSize;
+        skipSize++;
+      }
+    }
+
+    int blocks[16];
+    for (int i = 0; i < 16; i++) {
+      int blockStartIdx = i * 16;
+      int blockEndIdx = blockStartIdx + 16;
+
+      int block = 0;
+      for(int j = blockStartIdx; j < blockEndIdx; j++){
+        block ^= circle[j];        
+      }
+
+      blocks[i] = block;
+    }
+
+    std::string res = "";
+    for (auto block : blocks) {
+      auto hex = intToHex(block);
+      printf("%d -> %s\n", block, hex.c_str());
+      res += hex;
+    }
+
+    return res;
+  }
 
  private:
   std::vector<int> parseInput(const std::string& input) {
@@ -46,5 +108,16 @@ class Day10 : public AbstractTask {
     }
 
     return lengths;
+  }
+
+  std::string intToHex(int val) {
+    std::ostringstream ss;
+    ss << std::hex << val;
+    auto hexVal = ss.str();
+    if(hexVal.size() == 1) {
+      return "0" + hexVal;
+    }
+
+    return hexVal;
   }
 };
