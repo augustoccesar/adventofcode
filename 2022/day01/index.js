b41ff47b6ddf9d529ca352d60a98ce2616a7a3bf
Augusto César <augusto@augustocesar.se>
2022-12-01 14:51:30 -0300

    2022(1): small refactoring

--- 2022/day01/index.js
+++ 2022/day01/index.js
@@ -4,7 +4,7 @@ import { readInput } from "../input.js";
  * @returns {Promise<string>}
  */
 export async function partOne() {
-  const elfs = await resolveInput();
+  const elfs = await caloriesPerElf();
   let max = 0;
   elfs.forEach(elf => {
     if (elf > max) {
@@ -19,7 +19,7 @@ export async function partOne() {
  * @returns {Promise<string>}
  */
 export async function partTwo() {
-  const elfs = await resolveInput();
+  const elfs = await caloriesPerElf();
   const topElfs = elfs.sort((a, b) => b - a).slice(0, 3);
 
   return topElfs
@@ -27,19 +27,16 @@ export async function partTwo() {
     .toString();
 }
 
-async function resolveInput() {
-  const rawData = await readInput("01");
-  const groups = rawData.split("\n\n");
-
-  const elfs = [];
-  groups.forEach(group => {
-    const groupSum = group
-      .split("\n")
-      .map(item => parseInt(item))
-      .reduce((partialSum, item) => partialSum + item);
-
-    elfs.push(groupSum);
-  });
-
-  return elfs;
+/**
+ * @returns {Promise<Array<number>>}
+ */
+async function caloriesPerElf() {
+  return (await readInput("01"))
+    .split("\n\n")
+    .map(group => {
+      return group
+        .split("\n")
+        .map(item => parseInt(item))
+        .reduce((partialSum, item) => partialSum + item);
+    });
 }
