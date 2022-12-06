0cdef53158db84b0b07ee5553e3bdeb9712576bb
Augusto César <augusto@augustocesar.se>
2022-12-06 08:34:10 +0100

    2022(4): remove some duplication

--- 2022/src/main/kotlin/se/augustocesar/day04/Day04.kt
+++ 2022/src/main/kotlin/se/augustocesar/day04/Day04.kt
@@ -4,36 +4,38 @@ import se.augustocesar.Task
 
 class Day04 : Task() {
     override fun partOne(): String {
-        var total = 0
-
-        readInput().lines().forEach {line ->
-            val sections = line
-                .split(",")
-                .map { section -> section.split("-").map { item -> item.toInt() } }
-                .map { IntRange(it[0], it[1]) }
-
-            if(sections[0].subtract(sections[1]).isEmpty() || sections[1].subtract(sections[0]).isEmpty()) {
-                total++
-            }
-        }
-
-        return total.toString()
+        return run(readInput(), true).toString()
     }
 
     override fun partTwo(): String {
-        var total = 0
-
-        readInput().lines().forEach {line ->
-            val sections = line
-                .split(",")
-                .map { section -> section.split("-").map { item -> item.toInt() } }
-                .map { IntRange(it[0], it[1]) }
-
-            if(sections[0].intersect(sections[1]).isNotEmpty()) {
-                total++
-            }
-        }
-
-        return total.toString()
+        return run(readInput(), false).toString()
     }
+}
+
+private fun run(input: String, fullIntersectOnly: Boolean): Int {
+    var total = 0
+
+    input.lines().forEach { line ->
+        val sections = lineIntoRanges(line)
+
+        if (checkIntersect(sections[0], sections[1], fullIntersectOnly)) {
+            total++
+        }
+    }
+
+    return total
+}
+
+private fun checkIntersect(left: IntRange, right: IntRange, fullOnly: Boolean): Boolean {
+    return when (fullOnly) {
+        true -> left.subtract(right).isEmpty() || right.subtract(left).isEmpty()
+        false -> left.intersect(right).isNotEmpty()
+    }
+}
+
+private fun lineIntoRanges(line: String): List<IntRange> {
+    return line
+        .split(",")
+        .map { section -> section.split("-").map { item -> item.toInt() } }
+        .map { IntRange(it[0], it[1]) }
 }
