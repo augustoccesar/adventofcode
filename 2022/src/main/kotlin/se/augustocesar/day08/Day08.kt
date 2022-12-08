ec165821a70195764d0d4280f8fbd66d9addd401
Augusto César <augusto@augustocesar.se>
2022-12-08 23:20:36 +0100

    2022(8): solved part two

--- 2022/src/main/kotlin/se/augustocesar/day08/Day08.kt
+++ 2022/src/main/kotlin/se/augustocesar/day08/Day08.kt
@@ -25,8 +25,76 @@ class Day08 : Task() {
     }
 
     override fun partTwo(): String {
-        return "-"
+        val lines = readInput().lines()
+        val grid = arrayListOf<List<Int>>()
+
+        lines.forEach { line ->
+            grid.add(line.chars().map { it.toChar().digitToInt() }.toList())
+        }
+
+        var maxScenic = 0
+        for (row in 0 until grid.size) {
+            for (col in 0 until grid[0].size) {
+                val score = scenicScore(row, col, grid)
+                if (score > maxScenic) {
+                    maxScenic = score
+                }
+            }
+        }
+
+        return maxScenic.toString()
     }
+}
+
+fun scenicScore(row: Int, col: Int, map: List<List<Int>>): Int {
+    val size = map[row][col]
+
+    var visibleLeft = 0
+    var visibleRight = 0
+    var visibleUp = 0
+    var visibleDown = 0
+
+    // Look left
+    for (c in (col - 1) downTo 0) {
+        if (map[row][c] < size) {
+            visibleLeft++
+        } else {
+            visibleLeft++
+            break
+        }
+    }
+
+    // Look up
+    for (r in (row - 1) downTo 0) {
+        if (map[r][col] < size) {
+            visibleUp++
+        } else {
+            visibleUp++
+            break
+        }
+    }
+
+    // Look right
+    for (c in (col + 1) until map[0].size) {
+        if (map[row][c] < size) {
+            visibleRight++
+        } else {
+            visibleRight++
+            break
+        }
+    }
+
+    // Look down
+    for (r in (row + 1) until map.size) {
+        if (map[r][col] < size) {
+            visibleDown++
+        } else {
+            visibleDown++
+            break
+        }
+    }
+
+    return visibleLeft * visibleRight * visibleUp * visibleDown
 }
 
 fun visible(row: Int, col: Int, map: List<List<Int>>): Boolean {
