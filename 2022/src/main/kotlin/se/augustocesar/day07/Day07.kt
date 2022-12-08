5ae095c376fd188d411073a8d9dfc519675831c1
Augusto César <augusto@augustocesar.se>
2022-12-08 21:39:27 +0100

    2022(7): solved part two

--- 2022/src/main/kotlin/se/augustocesar/day07/Day07.kt
+++ 2022/src/main/kotlin/se/augustocesar/day07/Day07.kt
@@ -6,7 +6,7 @@ class Day07 : Task() {
     override fun partOne(): String {
         var readingDir: Entry? = null
         var currentDir = Entry("/", EntryType.FOLDER, null, arrayListOf(), 0)
-        var rootDir = currentDir
+        val rootDir = currentDir
 
         val lines = readInput().lines()
         for (i in 1 until lines.size) {
@@ -45,13 +45,22 @@ class Day07 : Task() {
         }
 
         rootDir.calculateSize()
-        val res = lookupDirs(rootDir, 100_000)
+        val res = lookupDirs(rootDir) { size -> size <= 100_000 }
 
         return res.sumOf { it.size }.toString()
     }
 
     override fun partTwo(): String {
-        return "-"
+        val totalStorage = 70_000_000
+        val updateSize = 30_000_000
+        val root = resolveFileSystem(readInput())
+
+        val storageAvailable = totalStorage - root.size
+        val extraStorageNeeded = updateSize - storageAvailable
+
+        val res = lookupDirs(root) { size -> size >= extraStorageNeeded }
+
+        return res.minOf { it.size }.toString()
     }
 }
 
@@ -76,16 +85,62 @@ class Entry(val name: String, val type: EntryType, val parent: Entry?, val child
     }
 }
 
-private fun lookupDirs(entry: Entry, maxSize: Int): List<Entry> {
+private fun resolveFileSystem(input: String): Entry {
+    var readingDir: Entry? = null
+    var currentDir = Entry("/", EntryType.FOLDER, null, arrayListOf(), 0)
+    val rootDir = currentDir
+
+    for((i, line) in input.lines().withIndex()) {
+        if(i == 0) {
+            continue
+        }
+
+        if (line.startsWith("$ cd")) {
+            val folderName = line.removePrefix("$ cd ")
+
+            val children = if (folderName == "..") {
+                currentDir.parent ?: throw RuntimeException("Wut")
+            } else {
+                currentDir.children.find { it.name == folderName } ?: throw RuntimeException("Wut")
+            }
+
+            currentDir = children
+            readingDir = null
+            continue
+        }
+
+        if (readingDir != null) {
+            if (line.startsWith("dir")) {
+                val dirName = line.removePrefix("dir ")
+                currentDir.children.add(Entry(dirName, EntryType.FOLDER, currentDir, arrayListOf(), 0))
+            } else {
+                val (size, fileName) = line.split(" ")
+                currentDir.children.add(Entry(fileName, EntryType.FILE, currentDir, arrayListOf(), size.toInt()))
+            }
+
+            continue
+        }
+
+        if (line.startsWith("$ ls")) {
+            readingDir = currentDir
+            continue
+        }
+    }
+
+    rootDir.calculateSize()
+    return rootDir
+}
+
+private fun lookupDirs(entry: Entry, fn: (Int) -> Boolean): List<Entry> {
     val folders: ArrayList<Entry> = arrayListOf()
 
-    if(entry.size <= maxSize) {
+    if(fn(entry.size)) {
         folders.add(entry)
     }
 
     entry.children.forEach {children ->
         if(children.type == EntryType.FOLDER) {
-            folders.addAll(lookupDirs(children, maxSize))
+            folders.addAll(lookupDirs(children, fn))
         }
     }
 
