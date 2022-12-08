fce16097ae4d587028a9f33cfbbb0503ff9c0480
Augusto César <augusto@augustocesar.se>
2022-12-08 19:16:06 +0100

    2022(6): remove code duplication

--- 2022/src/main/kotlin/se/augustocesar/day06/Day06.kt
+++ 2022/src/main/kotlin/se/augustocesar/day06/Day06.kt
@@ -5,31 +5,27 @@ import kotlin.streams.toList
 
 class Day06 : Task() {
     override fun partOne(): String {
-        val chars = readInput().chars().toList()
-
-        for (i in 3 until chars.size) {
-            if (setOf(chars[i], chars[i - 1], chars[i - 2], chars[i - 3]).size == 4) {
-                return (i + 1).toString()
-            }
-        }
-
-        throw RuntimeException("Unreachable")
+        return lookForMarker(readInput(), 4)?.toString().orEmpty()
     }
 
     override fun partTwo(): String {
-        val chars = readInput().chars().toList()
+        return lookForMarker(readInput(), 14)?.toString().orEmpty()
+    }
+}
 
-        for (i in 13 until chars.size) {
-            val checkChars = HashSet<Int>()
-            for (j in 0 until 14) {
-                checkChars.add(chars[i - j])
-            }
+private fun lookForMarker(message: String, size: Int): Int? {
+    val chars = message.chars().toList()
 
-            if(checkChars.size == 14) {
-                return (i + 1).toString()
-            }
+    for (i in (size - 1) until chars.size) {
+        val checkChars = HashSet<Int>()
+        for (j in 0 until size) {
+            checkChars.add(chars[i - j])
         }
 
-        throw RuntimeException("Unreachable")
+        if(checkChars.size == size) {
+            return i + 1
+        }
     }
+
+    return null
 }
