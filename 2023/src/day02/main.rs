d27e79f5d77baa850b9d8374c83ecbe379850645
Augusto César <augusto@augustocesar.se>
2023-12-02 11:59:17 -0300

    2023(2): solved part two

--- 2023/src/day02/main.rs
+++ 2023/src/day02/main.rs
@@ -1,6 +1,6 @@
 use std::convert::TryInto;
 
-use aoc2023::{read_input, read_named_input, timed};
+use aoc2023::{read_input, timed};
 
 fn part_one() -> String {
     let (config_red, config_green, config_blue) = (12, 13, 14);
@@ -47,7 +47,39 @@ fn part_one() -> String {
 }
 
 fn part_two() -> String {
-    String::from("part two")
+    let result = read_input("02")
+        .lines()
+        .map(|item| {
+            let parts = item.split(':').collect::<Vec<&str>>();
+
+            let [mut max_red, mut max_green, mut max_blue]: [_; 3] = [0, 0, 0];
+            let rounds = parts.get(1).unwrap().split(';').collect::<Vec<&str>>();
+            for round in rounds {
+                let picks = round.split(',').map(|pick| pick.trim());
+                for pick in picks {
+                    let [quantity, color]: [_; 2] =
+                        pick.split(' ').collect::<Vec<&str>>().try_into().unwrap();
+                    let quantity = quantity.parse::<u64>().unwrap();
+
+                    if color == "red" && quantity > max_red {
+                        max_red = quantity;
+                    }
+
+                    if color == "green" && quantity > max_green {
+                        max_green = quantity;
+                    }
+
+                    if color == "blue" && quantity > max_blue {
+                        max_blue = quantity;
+                    }
+                }
+            }
+
+            max_red * max_green * max_blue
+        })
+        .sum::<u64>();
+
+    result.to_string()
 }
 
 fn main() {
