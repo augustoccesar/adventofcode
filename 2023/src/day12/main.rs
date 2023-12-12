150ce25604d5c5cc70d2ae66f6186063d5496417
Augusto César <augusto@augustocesar.se>
2023-12-12 16:30:49 -0300

    2023(12): solved part two

--- 2023/src/day12/main.rs
+++ 2023/src/day12/main.rs
@@ -1,59 +1,52 @@
-use std::thread;
+use std::collections::HashMap;
 
-use aoc2023::{read_input, read_named_input, timed};
-use regex::Regex;
+use aoc2023::{read_input, timed};
 
 fn part_one() -> String {
-    let rows: Vec<(String, Vec<i32>)> = read_input("12")
+    let sum = read_input("12")
         .lines()
         .map(|line| {
-            let parts = line.split(' ').collect::<Vec<&str>>();
-            let format = parts[1]
-                .split(',')
-                .map(|item| item.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>();
-
-            (parts[0].to_string(), format)
+            let (spring, groups) = line.split_once(' ').unwrap();
+            (
+                spring.as_bytes(),
+                groups
+                    .split(',')
+                    .map(|group| group.parse::<usize>().unwrap())
+                    .collect::<Vec<usize>>(),
+            )
         })
-        .collect();
+        .map(|(spring, groups)| {
+            let mut cache: HashMap<String, usize> = HashMap::new();
+            count_options(spring, None, &groups, &mut cache)
+        })
+        .sum::<usize>();
 
-    let mut total_count = 0;
-    let (tx, rx) = std::sync::mpsc::channel::<u64>();
-
-    for row in &rows {
-        let packed_size: i32 = row.1.iter().sum::<i32>() + row.1.len() as i32 - 1;
-        let mut packed_version: Vec<char> = vec!['.'; packed_size as usize];
-
-        let mut current_pos = 0;
-        for format_group_size in &row.1 {
-            for _ in 0..*format_group_size {
-                packed_version[current_pos] = '#';
-
-                current_pos += 1;
-            }
-
-            current_pos += 1;
-        }
-
-        let tx_copy = tx.clone();
-        let row_copy = row.0.clone();
-        thread::spawn(move || {
-            let compact_row = compact_row(&row_copy.chars().collect::<Vec<char>>());
-            let count = find_options(&compact_row, &packed_version);
-
-            tx_copy.send(count).unwrap();
-        });
-    }
-
-    for _ in 0..rows.len() {
-        total_count += rx.recv().unwrap();
-    }
-
-    total_count.to_string()
+    sum.to_string()
 }
 
 fn part_two() -> String {
-    String::from("part two")
+    let sum = read_input("12")
+        .lines()
+        .map(|line| {
+            let (spring, groups) = line.split_once(' ').unwrap();
+            let spring = format!("{spring}?{spring}?{spring}?{spring}?{spring}");
+            let groups = format!("{groups},{groups},{groups},{groups},{groups}");
+
+            (
+                spring,
+                groups
+                    .split(',')
+                    .map(|group| group.parse::<usize>().unwrap())
+                    .collect::<Vec<usize>>(),
+            )
+        })
+        .map(|(spring, groups)| {
+            let mut cache: HashMap<String, usize> = HashMap::new();
+            count_options(spring.as_bytes(), None, &groups, &mut cache)
+        })
+        .sum::<usize>();
+
+    sum.to_string()
 }
 
 fn main() {
@@ -61,49 +54,58 @@ fn main() {
     timed(part_two);
 }
 
-fn compact_row(row: &[char]) -> Vec<char> {
-    let re = Regex::new(r"\.+").unwrap();
-    re.replace_all(&row.iter().collect::<String>(), ".")
-        .trim_matches('.')
-        .chars()
-        .collect::<Vec<char>>()
-}
+fn count_options(
+    springs: &[u8],
+    current_group_size: Option<usize>,
+    groups: &[usize],
+    cache: &mut HashMap<String, usize>,
+) -> usize {
+    let cache_key = format!("{:?}{:?}{:?}", springs, current_group_size, groups);
+    if cache.contains_key(&cache_key) {
+        return *cache.get(&cache_key).unwrap();
+    }
 
-fn find_options(compacted_row: &[char], compacted_format: &[char]) -> u64 {
-    for (i, c) in compacted_row.iter().enumerate() {
-        match c {
-            '?' => {
-                let mut option_1 = vec!['.'; compacted_row.len()];
-                option_1.copy_from_slice(compacted_row);
-                option_1[i] = '.';
-                let option_1 = compact_row(&option_1);
+    if springs.is_empty() {
+        let result = match current_group_size {
+            Some(group_size) if groups == [group_size] => 1,
+            None if groups.is_empty() => 1,
+            _ => 0,
+        };
 
-                let mut option_2 = vec!['.'; compacted_row.len()];
-                option_2.copy_from_slice(compacted_row);
-                option_2[i] = '#';
-                let option_2 = compact_row(&option_2);
+        cache.insert(cache_key, result);
+        return result;
+    }
 
-                let mut count = 0_u64;
-                count += find_options(&option_1, compacted_format);
-                count += find_options(&option_2, compacted_format);
-                return count;
-            }
-            '.' | '#' => {
-                if i > compacted_format.len() - 1 {
-                    continue;
-                }
+    let result = match (springs[0], current_group_size, groups) {
+        // Found '.', is outside of group: just move on
+        (b'.', None, _) => count_options(&springs[1..], None, groups, cache),
 
-                if compacted_row[i] != compacted_format[i] {
-                    return 0;
-                }
-            }
-            _ => unreachable!(),
+        // Found '?', is outside of group and there are no groups left: just move on
+        (b'?', None, []) => count_options(&springs[1..], None, groups, cache),
+
+        // Found '.' or '?', is inside a group and the group is the size of the current group: get out of group and remove the group from the list
+        (b'.' | b'?', Some(current_size), [group_size, ..]) if current_size == *group_size => {
+            count_options(&springs[1..], None, &groups[1..], cache)
         }
-    }
 
-    if compacted_row == compacted_format {
-        1
-    } else {
-        0
-    }
+        // Found '#' or `?`, is inside a group and the current size is smaller than the current group: increment the current size and move on
+        (b'#' | b'?', Some(current_size), [group_size, ..]) if current_size < *group_size => {
+            count_options(&springs[1..], Some(current_size + 1), groups, cache)
+        }
+
+        // Found `#` outside of a group and there are groups to solve: increment group size and move on
+        (b'#', None, [_, ..]) => count_options(&springs[1..], Some(1), groups, cache),
+
+        // Found `?`, outside of a group, and there are groups to solve: split into two different, one inside a group (to cover for '#')
+        // and one outside of a group (to cover for `.`)
+        (b'?', None, _) => {
+            count_options(&springs[1..], None, groups, cache)
+                + count_options(&springs[1..], Some(1), groups, cache)
+        }
+        _ => 0,
+    };
+
+    cache.insert(cache_key, result);
+
+    result
 }
