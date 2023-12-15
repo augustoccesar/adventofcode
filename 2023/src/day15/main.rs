30d35de112311510345d7f015f62b2b29f1269e2
Augusto César <augusto@augustocesar.se>
2023-12-15 09:57:48 -0300

    2023(15): solved part two

--- 2023/src/day15/main.rs
+++ 2023/src/day15/main.rs
@@ -1,4 +1,4 @@
-use aoc2023::{read_input, read_named_input, timed};
+use aoc2023::{read_input, timed};
 
 fn part_one() -> String {
     read_input("15")
@@ -12,7 +12,57 @@ fn part_one() -> String {
 }
 
 fn part_two() -> String {
-    String::from("part two")
+    let mut boxes: Vec<Vec<(String, u8)>> = vec![Vec::new(); 256];
+
+    read_input("15")
+        .lines()
+        .next()
+        .unwrap()
+        .split(',')
+        .for_each(|item| {
+            if item.contains('-') {
+                let tokens = item.split('-').collect::<Vec<_>>();
+                let label = tokens[0];
+                let box_index = hash(label) as usize;
+
+                for (i, (key, _)) in boxes[box_index].iter().enumerate() {
+                    if key == label {
+                        boxes[box_index].remove(i);
+                        break;
+                    }
+                }
+            } else if item.contains('=') {
+                let tokens = item.split('=').collect::<Vec<_>>();
+                let label = tokens[0];
+                let box_index = hash(label) as usize;
+                let focal_length = tokens[1].parse::<u8>().unwrap();
+
+                let mut replaced = false;
+                for (i, (key, _)) in boxes[box_index].iter().enumerate() {
+                    if key == label {
+                        boxes[box_index][i].1 = focal_length;
+                        replaced = true;
+                        break;
+                    }
+                }
+
+                if !replaced {
+                    boxes[box_index].push((label.to_string(), focal_length));
+                }
+            } else {
+                unreachable!()
+            };
+        });
+
+    let mut lenses_focus_power = 0;
+    for (box_index, r#box) in boxes.iter().enumerate() {
+        for (slot_index, lens) in r#box.iter().enumerate() {
+            let focusing_power = (box_index + 1) * (slot_index + 1) * lens.1 as usize;
+            lenses_focus_power += focusing_power;
+        }
+    }
+
+    lenses_focus_power.to_string()
 }
 
 fn main() {
