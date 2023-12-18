9512de94c1acec70ac91e1258b62d1cb0edc48cf
Augusto César <augusto@augustocesar.se>
2023-12-18 11:32:12 -0300

    2023(18): remove duplicated code

--- 2023/src/day18/main.rs
+++ 2023/src/day18/main.rs
@@ -1,51 +1,44 @@
 use aoc2023::{read_input, timed};
 
 fn part_one() -> String {
-    let instructions = read_input("18")
-        .lines()
-        .map(|line| {
-            let tokens = line.split(' ').collect::<Vec<_>>();
-            let direction = Direction::from_char(tokens[0].chars().next().unwrap());
-            let steps = tokens[1].parse::<i32>().unwrap();
-
-            (direction, steps)
-        })
-        .collect::<Vec<_>>();
-
-    // https://artofproblemsolving.com/wiki/index.php/Shoelace_Theorem
-    let mut area = 0;
-    let mut perimeter = 0;
-    let mut current_position = (0, 0);
-    for instruction in instructions {
-        let modifier = instruction.0.modifier();
-        let next_position = (
-            current_position.0 + (modifier.0 * instruction.1),
-            current_position.1 + (modifier.1 * instruction.1),
-        );
-
-        area += (current_position.1 + next_position.1) * (next_position.0 - current_position.0);
-        perimeter += instruction.1;
-
-        current_position = next_position;
-    }
-
-    ((area / 2).abs() + (perimeter / 2) + 1).to_string()
+    calculate_area(&parse_input(&read_input("18"), 1)).to_string()
 }
 
 fn part_two() -> String {
-    let instructions = read_input("18")
+    calculate_area(&parse_input(&read_input("18"), 2)).to_string()
+}
+
+fn main() {
+    timed(part_one);
+    timed(part_two);
+}
+
+fn parse_input(input: &str, part: u8) -> Vec<(Direction, i64)> {
+    input
         .lines()
-        .map(|line| {
-            let tokens = line.split('#').collect::<Vec<_>>();
-            let hex = &tokens[1][0..tokens[1].len() - 1];
-            let steps = i64::from_str_radix(&hex[0..hex.len() - 1], 16).unwrap();
-            let direction =
-                Direction::from(u8::from_str_radix(&hex[hex.len() - 1..hex.len()], 10).unwrap());
+        .map(|line| match part {
+            1 => {
+                let tokens = line.split(' ').collect::<Vec<_>>();
+                let direction = Direction::from_char(tokens[0].chars().next().unwrap());
+                let steps = tokens[1].parse::<i64>().unwrap();
 
-            (direction, steps)
+                (direction, steps)
+            }
+            2 => {
+                let tokens = line.split('#').collect::<Vec<_>>();
+                let hex = &tokens[1][0..tokens[1].len() - 1];
+                let steps = i64::from_str_radix(&hex[0..hex.len() - 1], 16).unwrap();
+                let direction =
+                    Direction::from((hex[hex.len() - 1..hex.len()]).parse::<u8>().unwrap());
+
+                (direction, steps)
+            }
+            _ => panic!("invalid part"),
         })
-        .collect::<Vec<_>>();
+        .collect::<Vec<_>>()
+}
 
+fn calculate_area(instructions: &[(Direction, i64)]) -> i64 {
     // https://artofproblemsolving.com/wiki/index.php/Shoelace_Theorem
     let mut area = 0;
     let mut perimeter = 0;
@@ -63,12 +56,7 @@ fn part_two() -> String {
         current_position = next_position;
     }
 
-    ((area / 2).abs() + (perimeter / 2) + 1).to_string()
-}
-
-fn main() {
-    timed(part_one);
-    timed(part_two);
+    (area / 2).abs() + (perimeter / 2) + 1
 }
 
 // TODO(augustoccesar)[2021-10-03]: Move this to a common module
