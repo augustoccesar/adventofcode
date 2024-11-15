3d4f3a0379ca1fb81ca4d3b603244e80775b9da0
Augusto César <augusto@augustocesar.se>
2024-11-15 10:01:09 -0300

    2022(misc): fix readLines method

--- 2022/src/input.zig
+++ 2022/src/input.zig
@@ -1,11 +1,15 @@
 const std = @import("std");
 
-pub fn readLines(allocator: std.mem.Allocator, input: []u8) [][]u8 {
-    var list = std.ArrayList([]u8).init(allocator);
+pub fn readLines(allocator: std.mem.Allocator, input: []u8) ![][]const u8 {
+    var list = std.ArrayList([]const u8).init(allocator);
     defer list.deinit();
 
-    const lines = std.mem.splitAny(u8, input, "\n");
-    return std.mem.bytesAsSlice([][]const u8, lines);
+    var lines = std.mem.splitAny(u8, input, "\n");
+    while (lines.next()) |line| {
+        try list.append(line);
+    }
+
+    return try list.toOwnedSlice();
 }
 
 pub fn linesIterator(input: []u8) std.mem.SplitIterator(u8, .any) {
