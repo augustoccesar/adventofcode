7495706f1624265bed0af1b926f11649f1f7b76d
Augusto César <augusto@augustocesar.se>
2024-11-15 00:09:47 -0300

    2022(4): unify parsing of line

--- 2022/src/day04/task.zig
+++ 2022/src/day04/task.zig
@@ -8,29 +8,7 @@ fn partOne(allocator: std.mem.Allocator, input: []u8) TaskError![]const u8 {
 
     var lines = linesIterator(input);
     while (lines.next()) |line| {
-        var assignments_raw: [2][2]u8 = [2][2]u8{
-            [2]u8{ 0, 0 },
-            [2]u8{ 0, 0 },
-        };
-
-        var sections = std.mem.splitAny(u8, line, ",");
-        var section_idx: usize = 0;
-        while (sections.next()) |section| {
-            var ids = std.mem.splitAny(u8, section, "-");
-            var id_idx: usize = 0;
-            while (ids.next()) |id| {
-                const value = try std.fmt.parseInt(u8, id, 10);
-
-                assignments_raw[section_idx][id_idx] = value;
-                id_idx += 1;
-            }
-            section_idx += 1;
-        }
-
-        const assignments: [2]Assignment = [2]Assignment{
-            Assignment{ .from = assignments_raw[0][0], .to = assignments_raw[0][1] },
-            Assignment{ .from = assignments_raw[1][0], .to = assignments_raw[1][1] },
-        };
+        const assignments = try parse_line(line);
 
         if (assignments[0].full_overlap(assignments[1])) {
             total += 1;
@@ -45,29 +23,7 @@ fn partTwo(allocator: std.mem.Allocator, input: []u8) TaskError![]const u8 {
 
     var lines = linesIterator(input);
     while (lines.next()) |line| {
-        var assignments_raw: [2][2]u8 = [2][2]u8{
-            [2]u8{ 0, 0 },
-            [2]u8{ 0, 0 },
-        };
-
-        var sections = std.mem.splitAny(u8, line, ",");
-        var section_idx: usize = 0;
-        while (sections.next()) |section| {
-            var ids = std.mem.splitAny(u8, section, "-");
-            var id_idx: usize = 0;
-            while (ids.next()) |id| {
-                const value = try std.fmt.parseInt(u8, id, 10);
-
-                assignments_raw[section_idx][id_idx] = value;
-                id_idx += 1;
-            }
-            section_idx += 1;
-        }
-
-        const assignments: [2]Assignment = [2]Assignment{
-            Assignment{ .from = assignments_raw[0][0], .to = assignments_raw[0][1] },
-            Assignment{ .from = assignments_raw[1][0], .to = assignments_raw[1][1] },
-        };
+        const assignments = try parse_line(line);
 
         if (assignments[0].any_overlap(assignments[1])) {
             total += 1;
@@ -103,7 +59,7 @@ const Assignment = struct {
         return sorted[1].from >= sorted[0].from and sorted[1].from <= sorted[0].to;
     }
 
-    fn sort(self: Assignment, other: Assignment) std.meta.Tuple(&.{ Assignment, Assignment }) {
+    fn sort(self: Assignment, other: Assignment) [2]Assignment {
         var left: Assignment = self;
         var right: Assignment = other;
 
@@ -118,3 +74,29 @@ const Assignment = struct {
         return .{ left, right };
     }
 };
+
+fn parse_line(line: []const u8) ![2]Assignment {
+    var assignments_raw: [2][2]u8 = [2][2]u8{
+        [2]u8{ 0, 0 },
+        [2]u8{ 0, 0 },
+    };
+
+    var sections = std.mem.splitAny(u8, line, ",");
+    var section_idx: usize = 0;
+    while (sections.next()) |section| {
+        var ids = std.mem.splitAny(u8, section, "-");
+        var id_idx: usize = 0;
+        while (ids.next()) |id| {
+            const value = try std.fmt.parseInt(u8, id, 10);
+
+            assignments_raw[section_idx][id_idx] = value;
+            id_idx += 1;
+        }
+        section_idx += 1;
+    }
+
+    return .{
+        .{ .from = assignments_raw[0][0], .to = assignments_raw[0][1] },
+        .{ .from = assignments_raw[1][0], .to = assignments_raw[1][1] },
+    };
+}
