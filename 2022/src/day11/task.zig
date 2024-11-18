c27e39744af4458ff85a315365e1916f622bea08
Augusto César <augusto@augustocesar.se>
2024-11-17 22:09:29 -0300

    2022(11): solved part two

--- 2022/src/day11/task.zig
+++ 2022/src/day11/task.zig
@@ -165,9 +165,65 @@ fn partOne(allocator: std.mem.Allocator, input_path: []u8) TaskError![]const u8 
 }
 
 fn partTwo(allocator: std.mem.Allocator, input_path: []u8) TaskError![]const u8 {
-    _ = input_path;
+    const input = helpers.input.readString(allocator, input_path);
+    var monkey_data = std.mem.splitSequence(u8, input, "\n\n");
 
-    return std.fmt.allocPrint(allocator, "-", .{});
+    var monkeys = std.ArrayList(*Monkey).init(allocator);
+    defer {
+        for (monkeys.items) |monkey| monkey.deinit();
+        monkeys.deinit();
+    }
+
+    var control_mod: u64 = 1;
+
+    while (monkey_data.next()) |data| {
+        const monkey = try Monkey.parse(allocator, data);
+
+        control_mod *= monkey.test_value;
+
+        try monkeys.append(monkey);
+    }
+
+    var inspections = try std.ArrayList(u64).initCapacity(allocator, monkeys.items.len);
+    defer inspections.deinit();
+    for (0..monkeys.items.len) |_| {
+        try inspections.append(0);
+    }
+
+    var round: u64 = 1;
+    while (true) {
+        for (monkeys.items) |monkey| {
+            while (monkey.holding_items.items.len > 0) {
+                const item = monkey.holding_items.orderedRemove(0);
+
+                inspections.items[monkey.idx] += 1;
+
+                const operation_target = monkey.operation.value orelse item;
+                const new_item = switch (monkey.operation.ty) {
+                    .add => (item + operation_target) % control_mod,
+                    .mult => (item * operation_target) % control_mod,
+                };
+
+                if (new_item % monkey.test_value == 0) {
+                    try monkeys.items[monkey.true_idx].holding_items.append(new_item);
+                } else {
+                    try monkeys.items[monkey.false_idx].holding_items.append(new_item);
+                }
+            }
+        }
+
+        if (round == 10_000) {
+            break;
+        }
+
+        round += 1;
+    }
+
+    const inspections_slice = inspections.items;
+    std.mem.sort(u64, inspections_slice, {}, comptime std.sort.desc(u64));
+    const result = inspections_slice[0] * inspections_slice[1];
+
+    return std.fmt.allocPrint(allocator, "{d}", .{result});
 }
 
 pub const task = Task{
