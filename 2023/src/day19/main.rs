0a444aa97bc559ad33af26ad45b8555ff22f374c
Augusto César <augusto@augustocesar.se>
2024-12-01 11:51:04 +0100

    2023(19): prepare day

---
+++ 2023/src/day19/main.rs
@@ -0,0 +1,14 @@
+use aoc2023::{read_input, read_named_input, timed};
+
+fn part_one() -> String {
+    String::from("part one")
+}
+
+fn part_two() -> String {
+    String::from("part two")
+}
+
+fn main() {
+    timed(part_one);
+    timed(part_two);
+}
