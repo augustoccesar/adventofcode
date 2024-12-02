3ec9134146ec28e9e2004268dbc622bae2145504
Augusto César <augusto@augustocesar.se>
2024-12-02 07:56:11 +0100

    2024(2): solved part one

--- 2024/day02/Day02.cs
+++ 2024/day02/Day02.cs
@@ -1,8 +1,52 @@
-class Day02: Task
+class Day02 : Task
 {
     public override string PartOne(string fileName)
     {
-        return "-";
+        var reports = Input.ReadLines(fileName).Select(line => line.Split(" ").ToList().ConvertAll(int.Parse));
+
+        var safeReports = 0;
+        foreach (var report in reports)
+        {
+            var direction = 0;
+
+            var safe = true;
+            for (var i = 1; i < report.Count; i++)
+            {
+                var prev = report[i - 1];
+                var curr = report[i];
+
+                if (curr == prev)
+                {
+                    safe = false;
+                    break;
+                };
+
+                var currDirection = curr > prev ? 1 : -1;
+
+                if (direction == 0)
+                {
+                    direction = currDirection;
+                }
+
+                if (direction != currDirection)
+                {
+                    safe = false;
+                    break;
+                }
+                else if (Math.Abs(curr - prev) > 3)
+                {
+                    safe = false;
+                    break;
+                }
+            }
+
+            if (safe)
+            {
+                safeReports += 1;
+            }
+        }
+
+        return safeReports.ToString();
     }
 
     public override string PartTwo(string fileName)
