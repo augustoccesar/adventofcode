030fae94aaa5208f91082c1bb03552cfb9610004
Augusto César <augusto@augustocesar.se>
2024-12-03 07:35:18 +0100

    2024(3): use partial for source-generated Regex

--- 2024/day03/Day03.cs
+++ 2024/day03/Day03.cs
@@ -1,15 +1,16 @@
 using System.Text.RegularExpressions;
 
-class Day03 : Task
+partial class Day03 : Task
 {
-    private static Regex MUL_PATTERN = new Regex(@"mul\((\d{1,3}),(\d{1,3})\)");
+    [GeneratedRegex(@"mul\((\d{1,3}),(\d{1,3})\)")]
+    private partial Regex MulPattern();
 
     public override string PartOne(string fileName)
     {
         var program = Input.ReadString(fileName);
 
         var total = 0;
-        foreach (Match match in MUL_PATTERN.Matches(program))
+        foreach (Match match in MulPattern().Matches(program))
         {
             total += int.Parse(match.Groups[1].ToString()) * int.Parse(match.Groups[2].ToString());
         }
@@ -40,7 +41,7 @@ class Day03 : Task
                 var maxIdx = Math.Min(i + 13, program.Length - 1);
                 var subString = program[i..maxIdx];
 
-                if (MUL_PATTERN.Match(subString) is var match && match.Length > 0)
+                if (MulPattern().Match(subString) is var match && match.Length > 0)
                 {
                     i += match.Length - 1;
 
