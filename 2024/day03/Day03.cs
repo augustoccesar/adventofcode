fea3613b65175a02ffa9bf6254db8607da5c1cfa
Augusto César <augusto@augustocesar.se>
2024-12-03 06:44:06 +0100

    2024(3): solved part one

--- 2024/day03/Day03.cs
+++ 2024/day03/Day03.cs
@@ -1,8 +1,18 @@
-class Day03: Task
+using System.Text.RegularExpressions;
+
+class Day03 : Task
 {
     public override string PartOne(string fileName)
     {
-        return "-";
+        var program = Input.ReadString(fileName);
+
+        var total = 0;
+        foreach (Match match in Regex.Matches(program, @"mul\((\d{1,3}),(\d{1,3})\)"))
+        {
+            total += int.Parse(match.Groups[1].ToString()) * int.Parse(match.Groups[2].ToString());
+        }
+
+        return total.ToString();
     }
 
     public override string PartTwo(string fileName)
