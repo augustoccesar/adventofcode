9631d97b58b327130b04229442bebdeb7d9e879d
Augusto César <augusto@augustocesar.se>
2024-12-04 07:31:21 +0100

    2024(4): create modifiers only once

--- 2024/day04/Day04.cs
+++ 2024/day04/Day04.cs
@@ -38,22 +38,27 @@ class Day04 : Task
         return result.ToString();
     }
 
+    private static readonly (int, int)[][] XMasModifersFromX = [
+        [(0, -1), (0, -2), (0, -3)],    // Up
+        [(1, -1), (2, -2), (3, -3)],    // Up Right
+        [(1, 0), (2, 0), (3, 0)],       // Right
+        [(1, 1), (2, 2), (3, 3)],       // Down Right
+        [(0, 1), (0, 2), (0, 3)],       // Down
+        [(-1, 1), (-2, 2), (-3, 3)],    // Down Left
+        [(-1, 0), (-2, 0), (-3, 0)],    // Left
+        [(-1, -1), (-2, -2), (-3, -3)], // Up Left
+    ];
+
+    private static readonly (int, int)[][] MasDiagonalsModifiers = [
+        [(-1, -1), (1, 1)], // Top Left -> Bottom Right
+        [(-1, 1), (1, -1)], // Bottom Right -> Top Right
+    ];
+
     private static int CountXmas(string[] input, (int, int) from)
     {
         int count = 0;
 
-        (int, int)[] up = [(0, -1), (0, -2), (0, -3)];
-        (int, int)[] upRight = [(1, -1), (2, -2), (3, -3)];
-        (int, int)[] right = [(1, 0), (2, 0), (3, 0)];
-        (int, int)[] downRight = [(1, 1), (2, 2), (3, 3)];
-        (int, int)[] down = [(0, 1), (0, 2), (0, 3)];
-        (int, int)[] downLeft = [(-1, 1), (-2, 2), (-3, 3)];
-        (int, int)[] left = [(-1, 0), (-2, 0), (-3, 0)];
-        (int, int)[] upLeft = [(-1, -1), (-2, -2), (-3, -3)];
-
-        (int, int)[][] possibleDirections = [up, upRight, right, downRight, down, downLeft, left, upLeft];
-
-        foreach (var direction in possibleDirections)
+        foreach (var direction in XMasModifersFromX)
         {
             try
             {
@@ -74,8 +79,8 @@ class Day04 : Task
 
     private static bool IsCrossedMas(string[] input, (int, int) from)
     {
-        (int, int)[] diag1 = [(-1, -1), (1, 1)];
-        (int, int)[] diag2 = [(-1, 1), (1, -1)];
+        (int, int)[] diag1 = MasDiagonalsModifiers[0];
+        (int, int)[] diag2 = MasDiagonalsModifiers[1];
 
         try
         {
