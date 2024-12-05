2060c382b34526622bf78b6f47084cd2cde3183e
Augusto César <augusto@augustocesar.se>
2024-12-05 07:39:11 +0100

    2024(5): solved part one

--- 2024/day05/Day05.cs
+++ 2024/day05/Day05.cs
@@ -1,8 +1,63 @@
-class Day05: Task
+class Day05 : Task
 {
     public override string PartOne(string fileName)
     {
-        return "-";
+        Dictionary<int, List<int>> rules = [];
+        List<List<int>> updates = [];
+
+        var parsingRules = true;
+        foreach (var line in Input.ReadLines(fileName))
+        {
+            if (line == "")
+            {
+                parsingRules = false;
+                continue;
+            }
+
+            if (parsingRules)
+            {
+                var pages = line.Split("|").ToList().ConvertAll(int.Parse);
+                if (rules.TryGetValue(pages[0], out var afterList))
+                {
+                    afterList.Add(pages[1]);
+                }
+                else
+                {
+                    rules[pages[0]] = [pages[1]];
+                }
+            }
+            else
+            {
+                updates.Add(line.Split(",").ToList().ConvertAll(int.Parse));
+            }
+        }
+
+        var result = 0;
+        foreach (var update in updates)
+        {
+            var isSorted = true;
+            for (var i = 1; i < update.Count && isSorted; i++)
+            {
+                if (rules.TryGetValue(update[i], out var smallerThan))
+                {
+                    for (var j = i - 1; j >= 0; j--)
+                    {
+                        if (smallerThan.Contains(update[j]))
+                        {
+                            isSorted = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (isSorted)
+            {
+                result += update[update.Count / 2];
+            }
+        }
+
+        return result.ToString();
     }
 
     public override string PartTwo(string fileName)
