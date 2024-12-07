2cedb9bd5f7e8f0a306eb2af849cf3ca168fb29f
Augusto César <augusto@augustocesar.se>
2024-12-07 15:23:52 +0100

    2024(7): solved part one

--- 2024/day07/Day07.cs
+++ 2024/day07/Day07.cs
@@ -2,11 +2,60 @@ class Day07 : Task
 {
     public override string PartOne(string fileName)
     {
-        return "-";
+        var result = 0L;
+
+        foreach (var line in Input.ReadLines(fileName))
+        {
+            var allValues = line.Split([' ', ':']).Where(c => c != "").ToList().ConvertAll(long.Parse);
+            var testValue = allValues[0];
+            var equationValues = allValues[1..];
+            var operationsPermutations = Permutations(equationValues.Count - 1);
+            foreach (var operationPermutation in operationsPermutations)
+            {
+                var acc = equationValues[0];
+                for (int i = 1; i < equationValues.Count; i++)
+                {
+                    var value = equationValues[i];
+                    var operationMask = 1 << (i - 1);
+                    if ((operationPermutation & operationMask) > 0)
+                    {
+                        acc *= value;
+                    }
+                    else
+                    {
+                        acc += value;
+                    }
+                }
+
+                if (acc == testValue)
+                {
+                    result += testValue;
+                    break;
+                }
+            }
+        }
+
+        return result.ToString();
     }
 
     public override string PartTwo(string fileName)
     {
         return "-";
+    }
+
+    private static List<int> Permutations(int count)
+    {
+        List<int> permutations = [];
+        int maxValue = ~(~0 << count);
+        permutations.Add(maxValue);
+
+        int currentValue = 0;
+        while (currentValue < maxValue)
+        {
+            permutations.Add(currentValue);
+            currentValue++;
+        }
+
+        return permutations;
     }
 }
