c1e285bd5ecbe56b0b5af73eb3d1db73c1bc17fc
Augusto César <augusto@augustocesar.se>
2024-12-08 17:24:13 +0100

    2024(8): solved part two

--- 2024/day08/Day08.cs
+++ 2024/day08/Day08.cs
@@ -52,7 +52,45 @@ class Day08 : Task
 
     public override string PartTwo(string fileName)
     {
-        return "-";
+        var map = Input.ReadLines(fileName).Select(line => line.ToCharArray()).ToArray();
+        var antenasLocations = new Dictionary<char, (int, int)[]>();
+        for (var y = 0; y < map.Length; y++)
+        {
+            for (var x = 0; x < map[0].Length; x++)
+            {
+                if (map[y][x] != '.')
+                {
+                    if (antenasLocations.TryGetValue(map[y][x], out var locations))
+                    {
+                        antenasLocations[map[y][x]] = [.. locations, (x, y)];
+                    }
+                    else
+                    {
+                        antenasLocations[map[y][x]] = [(x, y)];
+                    }
+                }
+            }
+        }
+
+        var antinodesPositions = new HashSet<(int, int)>();
+        foreach (var entry in antenasLocations)
+        {
+            if (entry.Value.Length > 1)
+            {
+                for (var i = 0; i < entry.Value.Length; i++)
+                {
+                    for (var j = i + 1; j < entry.Value.Length; j++)
+                    {
+                        var antinodesDir1 = AllAntinodesFromTo(map, entry.Value[i], entry.Value[j]);
+                        var antinodesDir2 = AllAntinodesFromTo(map, entry.Value[j], entry.Value[i]);
+
+                        antinodesPositions = [.. antinodesPositions, .. antinodesDir1, .. antinodesDir2];
+                    }
+                }
+            }
+        }
+
+        return antinodesPositions.Count.ToString();
     }
 
     private static bool IsPositionInRange((int, int) position, char[][] map)
@@ -62,9 +100,28 @@ class Day08 : Task
 
     private static (int, int) AntinodeOf((int, int) source, (int, int) target)
     {
-        var modX = source.Item1 - target.Item1;
-        var modY = source.Item2 - target.Item2;
+        var modX = target.Item1 - source.Item1;
+        var modY = target.Item2 - source.Item2;
 
-        return (source.Item1 + modX, source.Item2 + modY);
+        return (target.Item1 + modX, target.Item2 + modY);
+    }
+
+    private static HashSet<(int, int)> AllAntinodesFromTo(char[][] map, (int, int) source, (int, int) target)
+    {
+        HashSet<(int, int)> result = [];
+        result.Add(source);
+        result.Add(target);
+
+        var antinode = AntinodeOf(source, target);
+        while (IsPositionInRange(antinode, map))
+        {
+            result.Add(antinode);
+            source = target;
+            target = antinode;
+
+            antinode = AntinodeOf(source, target);
+        }
+
+        return result;
     }
 }
