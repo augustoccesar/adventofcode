13d4ea8d0c8c21af9edd99a669e4b3fe3e42ecbb
Augusto César <augusto@augustocesar.se>
2024-12-09 08:00:31 +0100

    2024(9): wip part two

--- 2024/day09/Day09.cs
+++ 2024/day09/Day09.cs
@@ -42,6 +42,55 @@ class Day09 : Task
 
     public override string PartTwo(string fileName)
     {
+        var diskmap = Input.ReadString(fileName).AsEnumerable().Select(c => c - '0').ToList();
+        var fileMap = new Dictionary<int, Stack<int>>();
+
+        var disk = new int?[diskmap.Sum()];
+        for (int i = 0, diskIdx = 0; i < diskmap.Count; i++)
+        {
+            if (i % 2 == 0)
+            {
+                if (!fileMap.ContainsKey(diskmap[i])) fileMap.Add(diskmap[i], new Stack<int>());
+
+                fileMap[diskmap[i]].Push(diskIdx);
+            }
+
+            for (int j = 0; j < diskmap[i]; j++, diskIdx++)
+            {
+                if (i % 2 == 0)
+                {
+                    disk[diskIdx] = i / 2;
+                }
+                else
+                {
+                    disk[diskIdx] = null;
+                }
+            }
+        }
+
+        for (int i = 0; i < disk.Length;)
+        {
+            if (disk[i] == null)
+            {
+                var startIdx = i;
+                var size = 0;
+                while (disk[i] == null)
+                {
+                    size += 1;
+                    i++;
+                }
+
+                var idxToMove = fileMap[size].Pop();
+
+                for (int j = startIdx; j < (startIdx + size); j++) disk[j] = disk[idxToMove];
+                for (int j = idxToMove; j < (idxToMove + size); j++) disk[j] = null;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
         return "-";
     }
 }
