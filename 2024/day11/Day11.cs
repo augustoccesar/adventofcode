e7de380aaefeb9bbf8dfa1a40b84950c1a971b58
Augusto César <augusto@augustocesar.se>
2024-12-11 07:53:03 +0100

    2024(11): solved part two

--- 2024/day11/Day11.cs
+++ 2024/day11/Day11.cs
@@ -36,6 +36,60 @@ class Day11 : Task
 
     public override string PartTwo(string fileName)
     {
-        return "-";
+        var stones = Input.ReadString(fileName).Split(" ").Select(long.Parse).ToList();
+        var stonesMap = new Dictionary<long, long>();
+        foreach (var stone in stones)
+        {
+            if (stonesMap.TryGetValue(stone, out long count))
+            {
+                stonesMap[stone] = count + 1;
+            }
+            else
+            {
+                stonesMap[stone] = 1;
+            }
+        }
+
+        for (int blinks = 0; blinks < 75; blinks++)
+        {
+            var newStonesMap = new Dictionary<long, long>();
+            foreach (var (stone, count) in stonesMap)
+            {
+                if (stone == 0)
+                {
+                    if (!newStonesMap.TryAdd(1, count))
+                    {
+                        newStonesMap[1] += count;
+                    }
+                }
+                else if (stone.ToString().Length % 2 == 0)
+                {
+                    var stoneStr = stone.ToString();
+                    var left = long.Parse(stoneStr[0..(stoneStr.Length / 2)]);
+                    var right = long.Parse(stoneStr[(stoneStr.Length / 2)..]);
+
+                    if (!newStonesMap.TryAdd(left, count))
+                    {
+                        newStonesMap[left] += count;
+                    }
+
+                    if (!newStonesMap.TryAdd(right, count))
+                    {
+                        newStonesMap[right] += count;
+                    }
+                }
+                else
+                {
+                    if (!newStonesMap.TryAdd(stone * 2024, count))
+                    {
+                        newStonesMap[stone * 2024] += count;
+                    }
+                }
+            }
+
+            stonesMap = newStonesMap;
+        }
+
+        return stonesMap.Values.Sum().ToString();
     }
 }
