9f8f722bf5a626447dd64044b6ebdfc20e143eb0
Augusto César <augusto@augustocesar.se>
2024-12-11 08:00:56 +0100

    2024(11): share code from part two into part one

--- 2024/day11/Day11.cs
+++ 2024/day11/Day11.cs
@@ -2,75 +2,61 @@ class Day11 : Task
 {
     public override string PartOne(string fileName)
     {
-        var stones = Input.ReadString(fileName).Split(" ").Select(long.Parse).ToList();
-
-        for (int blinks = 0; blinks < 25; blinks++)
-        {
-            var newStones = new List<long>(stones.Count * 2);
-            foreach (var stone in stones)
-            {
-                if (stone == 0)
-                {
-                    newStones.Add(1);
-                }
-                else if (stone.ToString().Length % 2 == 0)
-                {
-                    var stoneStr = stone.ToString();
-                    var left = stoneStr[0..(stoneStr.Length / 2)];
-                    var right = stoneStr[(stoneStr.Length / 2)..];
-
-                    newStones.Add(long.Parse(left));
-                    newStones.Add(long.Parse(right));
-                }
-                else
-                {
-                    newStones.Add(stone * 2024);
-                }
-            }
-
-            stones = newStones;
-        }
-
-        return stones.Count.ToString();
+        return Blink(fileName, 25).Values.Sum().ToString();
     }
 
     public override string PartTwo(string fileName)
     {
-        var stones = Input.ReadString(fileName).Split(" ").Select(long.Parse).ToList();
+        return Blink(fileName, 75).Values.Sum().ToString();
+    }
+
+    private static Dictionary<long, long> BuildStonesMap(string inputName)
+    {
+        var stones = Input.ReadString(inputName).Split(" ").Select(long.Parse).ToList();
         var stonesMap = new Dictionary<long, long>();
         foreach (var stone in stones)
         {
             InsertOrIncrement(ref stonesMap, stone, 1);
         }
 
-        for (int blinks = 0; blinks < 75; blinks++)
+        return stonesMap;
+    }
+
+    private static Dictionary<long, long> Blink(string inputName, int times)
+    {
+        var stonesMap = BuildStonesMap(inputName);
+        for (int i = 0; i < times; i++)
         {
-            var newStonesMap = new Dictionary<long, long>();
-            foreach (var (stone, count) in stonesMap)
+            stonesMap = IterateStonesMap(stonesMap);
+        }
+        return stonesMap;
+    }
+
+    private static Dictionary<long, long> IterateStonesMap(Dictionary<long, long> stonesMap)
+    {
+        var newStonesMap = new Dictionary<long, long>();
+        foreach (var (stone, count) in stonesMap)
+        {
+            if (stone == 0)
             {
-                if (stone == 0)
-                {
-                    InsertOrIncrement(ref newStonesMap, 1, count);
-                }
-                else if (stone.ToString().Length % 2 == 0)
-                {
-                    var stoneStr = stone.ToString();
-                    var left = long.Parse(stoneStr[0..(stoneStr.Length / 2)]);
-                    var right = long.Parse(stoneStr[(stoneStr.Length / 2)..]);
-
-                    InsertOrIncrement(ref newStonesMap, left, count);
-                    InsertOrIncrement(ref newStonesMap, right, count);
-                }
-                else
-                {
-                    InsertOrIncrement(ref newStonesMap, stone * 2024, count);
-                }
+                InsertOrIncrement(ref newStonesMap, 1, count);
             }
+            else if (stone.ToString().Length % 2 == 0)
+            {
+                var stoneStr = stone.ToString();
+                var left = long.Parse(stoneStr[0..(stoneStr.Length / 2)]);
+                var right = long.Parse(stoneStr[(stoneStr.Length / 2)..]);
 
-            stonesMap = newStonesMap;
+                InsertOrIncrement(ref newStonesMap, left, count);
+                InsertOrIncrement(ref newStonesMap, right, count);
+            }
+            else
+            {
+                InsertOrIncrement(ref newStonesMap, stone * 2024, count);
+            }
         }
 
-        return stonesMap.Values.Sum().ToString();
+        return newStonesMap;
     }
 
     private static void InsertOrIncrement(ref Dictionary<long, long> dict, long key, long value)
