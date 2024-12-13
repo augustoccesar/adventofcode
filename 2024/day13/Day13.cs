9b01b0d71c5a26f2a25447cf861f07e17c4c2940
Augusto César <augusto@augustocesar.se>
2024-12-13 07:34:04 +0100

    2024(13): prepare day

---
+++ 2024/day13/Day13.cs
@@ -0,0 +1,12 @@
+class Day13 : Task
+{
+    public override string PartOne(string fileName)
+    {
+        return "-";
+    }
+
+    public override string PartTwo(string fileName)
+    {
+        return "-";
+    }
+}
