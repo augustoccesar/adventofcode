4aac5fe9f53df8cce60f0d151b5c8b5cfdb42fdc
Augusto César <augusto@augustocesar.se>
2025-11-24 16:22:26 -0300

    feat(zig): add input helpers back

--- zig/src/day.zig
+++ zig/src/day.zig
@@ -14,25 +14,4 @@ pub const Day = struct {
     pub fn partTwo(self: Day, allocator: Allocator) ![]const u8 {
         return try self.part_two_fn(allocator);
     }
-
-    pub fn readInput(self: Day, allocator: Allocator, name: []const u8) ![]const u8 {
-        const path = if (name.len == 0)
-            try std.fmt.allocPrint(allocator, "../inputs/{d}_{d:0>2}.txt", .{ self.year, self.day })
-        else
-            try std.fmt.allocPrint(allocator, "../inputs/{d}_{d:0>2}_{s}.txt", .{ self.year, self.day, name });
-        defer allocator.free(path);
-
-        const file = std.fs.cwd().openFile(path, .{}) catch |err| {
-            std.debug.print("Error opening file {s}: {any}\n", .{ path, err });
-            return err;
-        };
-        defer file.close();
-
-        const content = try file.readToEndAlloc(allocator, std.math.maxInt(usize));
-        return content;
-    }
-
-    pub fn readDefaultInput(self: Day, allocator: Allocator) ![]const u8 {
-        return try self.readInput(allocator, "");
-    }
 };
