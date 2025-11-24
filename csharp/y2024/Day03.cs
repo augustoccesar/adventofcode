5c0ef9cddcef1c2f07f2c298685283c3e80034c9
Augusto César <augusto@augustocesar.se>
2025-11-24 16:56:35 -0300

    2024(3,cs): migrate

---
+++ csharp/y2024/Day03.cs
@@ -0,0 +1,79 @@
+using System.Text.RegularExpressions;
+
+namespace aoc.y2024;
+
+[RunnableDay(year: 2024, day: 3)]
+partial class Day03 : Day
+{
+    [GeneratedRegex(@"mul\((\d{1,3}),(\d{1,3})\)")]
+    private partial Regex MulPattern();
+
+    public override string PartOne()
+    {
+        var program = ReadInput();
+
+        var total = 0;
+        foreach (Match match in MulPattern().Matches(program))
+        {
+            total += int.Parse(match.Groups[1].ToString()) * int.Parse(match.Groups[2].ToString());
+        }
+
+        return total.ToString();
+    }
+
+    public override string PartTwo()
+    {
+        var program = ReadInput();
+
+        var enabled = true;
+
+        var total = 0;
+        for (int i = 0; i < program.Length; i++)
+        {
+            // instruction      | length
+            // ---------------- | ------
+            // mul(1, 2)        | 9
+            // mul(10, 20)      | 11
+            // mul(100, 200)    | 13
+            // do()             | 4
+            // don't()          | 7
+
+            var c = program[i];
+            if (c == 'm')
+            {
+                var maxIdx = Math.Min(i + 13, program.Length - 1);
+                var subString = program[i..maxIdx];
+
+                if (MulPattern().Match(subString) is var match && match.Length > 0)
+                {
+                    i += match.Length - 1;
+
+                    if (enabled)
+                    {
+                        total += int.Parse(match.Groups[1].ToString()) * int.Parse(match.Groups[2].ToString());
+                    }
+                }
+            }
+            else if (c == 'd')
+            {
+                var maxIdx = Math.Min(i + 7, program.Length - 1);
+                var subString = program[i..maxIdx];
+
+                if (subString.StartsWith("do()"))
+                {
+                    enabled = true;
+                    i += 4 - 1;
+                }
+                else if (subString.StartsWith("don't()"))
+                {
+                    enabled = false;
+                    i += 7 - 1;
+                }
+            }
+        }
+
+
+        return total.ToString();
+    }
+}
+
