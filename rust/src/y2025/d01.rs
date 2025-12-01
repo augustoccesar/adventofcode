9e0c962cf3cc92025584258c02766eaaf83055dd
Augusto César <augusto@augustocesar.se>
2025-12-01 18:45:59 -0300

    2025(1,rs): part two

--- rust/src/y2025/d01.rs
+++ rust/src/y2025/d01.rs
@@ -44,8 +44,6 @@ impl Day for Day01 {
                 position %= 100;
             }
 
-            println!("Rotating {rotation}. New position {position}");
-
             if position == 0 {
                 total += 1;
             }
@@ -55,6 +53,45 @@ impl Day for Day01 {
     }
 
     fn part_two(&self) -> String {
-        "-".to_owned()
+        let input = self.read_default_input();
+        let input = input.lines().map(|line| {
+            let chars = line.chars().collect::<Vec<_>>();
+
+            let number = chars[1..]
+                .iter()
+                .copied()
+                .collect::<String>()
+                .parse::<i32>()
+                .expect("Invalid format of line");
+
+            match &chars[0] {
+                'R' => number,
+                'L' => number * -1,
+                _ => unreachable!("Invalid format of line"),
+            }
+        });
+
+        let mut clicks = 0;
+        let mut position = 50;
+
+        // TODO: There must be a better way xD
+        for rotation in input {
+            let direction = if rotation > 0 { 1 } else { -1 };
+            for _ in 0..rotation.abs() {
+                position += direction;
+
+                if position == -1 {
+                    position = 99;
+                } else if position == 100 {
+                    position = 0;
+                }
+
+                if position == 0 {
+                    clicks += 1;
+                }
+            }
+        }
+
+        clicks.to_string()
     }
 }
