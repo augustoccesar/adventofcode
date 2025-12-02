46822d51d401bb939d6cc76e42204c32e4e89e1c
Augusto César <augusto@augustocesar.se>
2025-12-02 12:52:37 -0300

    2025(2,rs): part two

--- rust/src/y2025/d02.rs
+++ rust/src/y2025/d02.rs
@@ -65,10 +65,90 @@ impl Day for Day02 {
     }
 
     fn part_two(&self) -> String {
-        "-".to_owned()
+        let mut total = 0;
+
+        for mut range in self
+            .read_default_input()
+            .split(",")
+            .map(|range| range.split("-"))
+        {
+            let start = range
+                .next()
+                .expect("range to have at two parts. Missing first.")
+                .parse::<i64>()
+                .unwrap();
+            let end = range
+                .next()
+                .expect("range to have at two parts. Missing second.")
+                .parse::<i64>()
+                .unwrap();
+
+            'number_loop: for number in start..=end {
+                let number_digits = number.to_string().chars().collect::<Vec<char>>();
+
+                if number_digits.len() < 2 {
+                    continue;
+                }
+
+                if is_all_same(&number_digits) {
+                    total += number;
+                    continue;
+                }
+
+                for divisor in divisors(number_digits.len()) {
+                    let chunk_size = number_digits.len() / divisor;
+
+                    let groups = number_digits
+                        .chunks(chunk_size)
+                        .map(|chars| chars.iter().collect::<String>())
+                        .collect::<Vec<String>>();
+
+                    if is_all_same(&groups) {
+                        total += number;
+
+                        continue 'number_loop;
+                    }
+                }
+            }
+        }
+
+        total.to_string()
     }
 }
 
 fn int_len(integer: i64) -> usize {
     integer.to_string().len()
+}
+
+fn is_all_same<T>(arr: &[T]) -> bool
+where
+    T: PartialEq,
+{
+    if arr.is_empty() {
+        return true;
+    }
+
+    let first = &arr[0];
+    arr.iter().all(|item| item == first)
+}
+
+fn divisors(number: usize) -> Vec<usize> {
+    let mut divisors = Vec::new();
+    let limit = (number as f64).sqrt() as usize;
+
+    // By skipping the '1' it avoids adding 1 and the number itself.
+    // Since when we are grouping we don't want to group by the number and also
+    // we the groups must be bigger than 2, so this just those cases.
+    for i in 2..=limit {
+        if number % i == 0 {
+            divisors.push(i);
+
+            if i * i != number {
+                divisors.push(number / i);
+            }
+        }
+    }
+
+    divisors.sort();
+    divisors
 }
