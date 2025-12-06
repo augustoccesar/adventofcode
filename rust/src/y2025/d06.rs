8e23fc405a4a6baa4e2fd8915d2f1ab745ce4087
Augusto César <augusto@augustocesar.se>
2025-12-06 11:55:48 -0300

    2025(6,rs): part two

--- rust/src/y2025/d06.rs
+++ rust/src/y2025/d06.rs
@@ -58,6 +58,73 @@ impl Day for Day06 {
     }
 
     fn part_two(&self) -> String {
-        "-".to_owned()
+        let input = self.read_default_input();
+        let lines = input.lines().collect::<Vec<&str>>();
+
+        let mut data: Vec<Vec<char>> = vec![vec![]; lines[0].len()];
+
+        for line in &lines {
+            for (i, character) in line.chars().enumerate() {
+                data[i].push(character);
+            }
+        }
+
+        let mut total = 0;
+        let mut curr_problem_total = 0;
+        let mut curr_operation = None;
+        for scan in data {
+            // If the last char in the vertical scan is * or +, a new problem is starting.
+            match scan.last().expect("scan should not be empty") {
+                '*' => {
+                    curr_operation = Some(Operation::Mult);
+                    curr_problem_total = 1;
+                }
+                '+' => {
+                    curr_operation = Some(Operation::Add);
+                    curr_problem_total = 0;
+                }
+                _ => (),
+            }
+
+            let digits = scan
+                .iter()
+                .filter(|c| **c != ' ' && **c != '*' && **c != '+')
+                .map(|c| {
+                    c.to_digit(10)
+                        .expect("unfiltered characters should be a valid digit")
+                        as i64
+                })
+                .rev()
+                .collect::<Vec<_>>();
+
+            // The only vertical scan without any digits is the one before the start of a new
+            // problem, so add the total of the current problem to the aggregate total and
+            // move to the next vertical scan.
+            if digits.is_empty() {
+                total += curr_problem_total;
+
+                continue;
+            }
+
+            let mut number = 0i64;
+            for (i, digit) in digits.iter().enumerate() {
+                number += *digit * (10i64.pow(i as u32));
+            }
+
+            match curr_operation.clone().unwrap() {
+                Operation::Add => curr_problem_total += number,
+                Operation::Mult => curr_problem_total *= number,
+            }
+        }
+
+        total += curr_problem_total;
+
+        total.to_string()
     }
+}
+
+#[derive(Debug, Clone)]
+enum Operation {
+    Mult,
+    Add,
 }
